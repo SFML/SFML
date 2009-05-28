f2fe1cc4ca309a59bd77a822496adef2651961e7
laurentgom <laurentgom@4e206d99-4929-0410-ac5d-dfc041789085>
2009-05-28 15:32:20 +0000

    Updated copyrights
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/trunk@1108 4e206d99-4929-0410-ac5d-dfc041789085

--- CSFML/include/SFML/Window.h
+++ CSFML/include/SFML/Window.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007 Laurent Gomila (laurent.gom@gmail.com)
+// Copyright (C) 2007-2009 Laurent Gomila (laurent.gom@gmail.com)
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -33,6 +33,7 @@
 #include <SFML/Window/Context.h>
 #include <SFML/Window/Event.h>
 #include <SFML/Window/Input.h>
+#include <SFML/Window/OpenGL.h>
 #include <SFML/Window/VideoMode.h>
 #include <SFML/Window/Window.h>
 
