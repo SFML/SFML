e4165be2f9524b2ab1a80b06b26b9242d6816f9d
LaurentGom <LaurentGom@4e206d99-4929-0410-ac5d-dfc041789085>
2010-01-14 15:15:05 +0000

    Fixed the batch-build script for Code::Blocks
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/trunk@1359 4e206d99-4929-0410-ac5d-dfc041789085

--- build/codeblocks/batch-build/build.bat
+++ build/codeblocks/batch-build/build.bat
@@ -10,35 +10,37 @@ set PATH=%PATH%;"%CD%"
 REM Compile the SFML libraries
 cd %SFML%\build\codeblocks
 
+echo Compiling sfml-main...
+codeblocks -na -nd -ns --build sfml-main.cbp --target="Debug_Win32"
+codeblocks -na -nd -ns --build sfml-main.cbp --target="Release_Win32"
+
 echo Compiling debug static libraries...
-codeblocks -na -nd -ns --build sfml-main.cbp --target="Debug"
-codeblocks -na -nd -ns --build sfml-audio.cbp --target="Debug static_Win32"
-codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Debug static_Win32"
-codeblocks -na -nd -ns --build sfml-network.cbp --target="Debug static_Win32"
 codeblocks -na -nd -ns --build sfml-system.cbp --target="Debug static_Win32"
 codeblocks -na -nd -ns --build sfml-window.cbp --target="Debug static_Win32"
+codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Debug static_Win32"
+codeblocks -na -nd -ns --build sfml-audio.cbp --target="Debug static_Win32"
+codeblocks -na -nd -ns --build sfml-network.cbp --target="Debug static_Win32"
 
 echo Compiling debug dynamic libraries...
-codeblocks -na -nd -ns --build sfml-audio.cbp --target="Debug DLL_Win32"
-codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Debug DLL_Win32"
-codeblocks -na -nd -ns --build sfml-network.cbp --target="Debug DLL_Win32"
 codeblocks -na -nd -ns --build sfml-system.cbp --target="Debug DLL_Win32"
 codeblocks -na -nd -ns --build sfml-window.cbp --target="Debug DLL_Win32"
+codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Debug DLL_Win32"
+codeblocks -na -nd -ns --build sfml-audio.cbp --target="Debug DLL_Win32"
+codeblocks -na -nd -ns --build sfml-network.cbp --target="Debug DLL_Win32"
 
 echo Compiling release static libraries...
-codeblocks -na -nd -ns --build sfml-main.cbp --target="Release"
-codeblocks -na -nd -ns --build sfml-audio.cbp --target="Release static_Win32"
-codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Release static_Win32"
-codeblocks -na -nd -ns --build sfml-network.cbp --target="Release static_Win32"
 codeblocks -na -nd -ns --build sfml-system.cbp --target="Release static_Win32"
 codeblocks -na -nd -ns --build sfml-window.cbp --target="Release static_Win32"
+codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Release static_Win32"
+codeblocks -na -nd -ns --build sfml-audio.cbp --target="Release static_Win32"
+codeblocks -na -nd -ns --build sfml-network.cbp --target="Release static_Win32"
 
 echo Compiling release dynamic libraries...
-codeblocks -na -nd -ns --build sfml-audio.cbp --target="Release DLL_Win32"
-codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Release DLL_Win32"
-codeblocks -na -nd -ns --build sfml-network.cbp --target="Release DLL_Win32"
 codeblocks -na -nd -ns --build sfml-system.cbp --target="Release DLL_Win32"
 codeblocks -na -nd -ns --build sfml-window.cbp --target="Release DLL_Win32"
+codeblocks -na -nd -ns --build sfml-graphics.cbp --target="Release DLL_Win32"
+codeblocks -na -nd -ns --build sfml-network.cbp --target="Release DLL_Win32"
+codeblocks -na -nd -ns --build sfml-audio.cbp --target="Release DLL_Win32"
 
 REM Copy the SFML static libraries into a temporary folder
 IF NOT EXIST %TMP% mkdir %TMP%
@@ -56,13 +58,6 @@ ar rs libsfml-window-s.a *.o
 ar rs libsfml-window-s-d.a *.o
 del *.o /f /q
 
-REM Add external libraries to sfml-network
-echo Adding external libraries to libsfml-network-s...
-ar x %SFML%\build\codeblocks\batch-build\libws2_32.a
-ar rs libsfml-network-s.a *.o
-ar rs libsfml-network-s-d.a *.o
-del *.o /f /q
-
 REM Add external libraries to sfml-graphics
 echo Adding external libraries to libsfml-graphics-s...
 ar x %SFML%\extlibs\libs-mingw\libfreetype.a
@@ -76,6 +71,13 @@ ar x %SFML%\extlibs\libs-mingw\libopenal32.a
 ar x %SFML%\extlibs\libs-mingw\libsndfile.a
 ar rs libsfml-audio-s.a *.o
 ar rs libsfml-audio-s-d.a *.o
+del *.o /f /q
+
+REM Add external libraries to sfml-network
+echo Adding external libraries to libsfml-network-s...
+ar x %SFML%\build\codeblocks\batch-build\libws2_32.a
+ar rs libsfml-network-s.a *.o
+ar rs libsfml-network-s-d.a *.o
 del *.o /f /q
 
 REM Copy the SFML libraries to their final location
