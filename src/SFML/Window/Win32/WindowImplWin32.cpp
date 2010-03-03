dec1820537e3a5b42faf2c1d657398aa0cad177c
LaurentGom <LaurentGom@4e206d99-4929-0410-ac5d-dfc041789085>
2010-03-03 07:59:28 +0000

    Fixed issues with the fullscreen mode on Windows
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/trunk@1435 4e206d99-4929-0410-ac5d-dfc041789085

--- src/SFML/Window/Win32/WindowImplWin32.cpp
+++ src/SFML/Window/Win32/WindowImplWin32.cpp
@@ -481,13 +481,13 @@ void WindowImplWin32::SwitchToFullscreen(const VideoMode& Mode)
         return;
     }
 
-    // Resize the window so that it fits the entire screen
-    SetWindowPos(myHandle, HWND_TOP, 0, 0, Mode.Width, Mode.Height, SWP_FRAMECHANGED);
-    ShowWindow(myHandle, SW_SHOW);
-
     // Make the window flags compatible with fullscreen mode
     SetWindowLong(myHandle, GWL_STYLE, WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);
     SetWindowLong(myHandle, GWL_EXSTYLE, WS_EX_APPWINDOW);
+
+    // Resize the window so that it fits the entire screen
+    SetWindowPos(myHandle, HWND_TOP, 0, 0, Mode.Width, Mode.Height, SWP_FRAMECHANGED);
+    ShowWindow(myHandle, SW_SHOW);
 
     // Set "this" as the current fullscreen window
     ourFullscreenWindow = this;
