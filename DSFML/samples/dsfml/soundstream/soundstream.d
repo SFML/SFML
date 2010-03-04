4a192257641438fe16c2db65ccdce5a4663b0236
trass3r <trass3r@4e206d99-4929-0410-ac5d-dfc041789085>
2010-03-04 02:23:27 +0000

    * simply replacing spaces with tabs in the example files
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/branches/sfml2@1440 4e206d99-4929-0410-ac5d-dfc041789085

--- DSFML/samples/dsfml/soundstream/soundstream.d
+++ DSFML/samples/dsfml/soundstream/soundstream.d
@@ -5,12 +5,12 @@ import dsfml.audio.all;
 
 version (Tango)
 {
-    import tango.io.Console;
-    import tango.io.Stdout;
+	import tango.io.Console;
+	import tango.io.Stdout;
 }
 else
 {
-    import std.stdio;
+	import std.stdio;
 }
 
 // SoundStream is an abstract class.
@@ -18,74 +18,74 @@ else
 // Don't forget to call initialize() before any usage or playback will fail.
 class MySoundStream : SoundStream
 {
-    SoundBuffer m_buff;
-    short[] m_data;
-    size_t m_cursor;
-    
-    this()
-    {
-        // We initialize the stream with some sound informations
-        super(1, 11025);
-        
-        // We create a sound buffer to load samples from files
-        m_buff = new SoundBuffer("Data/car_idle.wav");
-        m_data = m_buff.getSamples[0..m_buff.getSamplesCount];
-    }
+	SoundBuffer m_buff;
+	short[] m_data;
+	size_t m_cursor;
+	
+	this()
+	{
+		// We initialize the stream with some sound informations
+		super(1, 11025);
+		
+		// We create a sound buffer to load samples from files
+		m_buff = new SoundBuffer("Data/car_idle.wav");
+		m_data = m_buff.getSamples[0..m_buff.getSamplesCount];
+	}
 protected:
-    bool onStart()
-    {
-        // No specifics things to do, just return true.
-        return true;
-    }
-    
-    bool onGetData(out short[] data)
-    {
-        // We ensure that we have enough data to send
-        if (m_cursor + this.getSampleRate > m_data.length)
-            return false;
-        
-        // Assign data in the buffer ...
-        data = m_data[m_cursor..m_cursor + this.getSampleRate];
-        // ... and increment the cursor
-        m_cursor += this.getSampleRate;
-        return true;
-    }
-    
+	bool onStart()
+	{
+		// No specifics things to do, just return true.
+		return true;
+	}
+	
+	bool onGetData(out short[] data)
+	{
+		// We ensure that we have enough data to send
+		if (m_cursor + this.getSampleRate > m_data.length)
+			return false;
+		
+		// Assign data in the buffer ...
+		data = m_data[m_cursor..m_cursor + this.getSampleRate];
+		// ... and increment the cursor
+		m_cursor += this.getSampleRate;
+		return true;
+	}
+	
 }
 
 void main()
 {
-    MySoundStream stream = new MySoundStream();
-    
-    display("Playing sound !\n Press enter to stop playback.");
-    stream.play();
-    read();
-    stream.stop();
+	MySoundStream stream = new MySoundStream();
+	
+	display("Playing sound !\n Press enter to stop playback.");
+	stream.play();
+	read();
+	stream.stop();
 }
 
 void display(char[] c)
 {
-    version (Tango)
+	version (Tango)
 { 
-        Stdout(c).newline;
+		Stdout(c).newline;
 }
-    else
+	else
 {
-        writefln("%s", c);
+		writefln("%s", c);
 }
 }
 
 /**
-*   Dummy function to prevent console closing on windows
+*	Dummy function to prevent console closing on windows
 */
 void read()
 {
-    version (Tango)
+	version (Tango)
 {
-        Cin.get();
+		Cin.get();
 }
-    else
+	else
 {
-        readln();
+		readln();
 }
 }
--- DSFML/samples/dsfml/voip/util.d
+++ DSFML/samples/dsfml/voip/util.d
@@ -1,6 +1,6 @@
 module util;
 
-const ubyte AudioData   = 1;
+const ubyte AudioData	= 1;
 const ubyte EndOfStream = 2;
 
 public import dsfml.system.all;
@@ -9,41 +9,41 @@ public import dsfml.network.all;
 
 version(Tango)
 {
-    public import tango.io.Console;
-    public import tango.text.convert.Integer;
+	public import tango.io.Console;
+	public import tango.text.convert.Integer;
 }
 else
 {
-    public import std.stdio;
-    
-    //simple abstraction of Cout & Cin for phobos    
-    class Cout
-    {
-        static Cout s_c;
-        static this()
-        {
-            s_c = new Cout();
-        }
-        
-        static Cout opCall(char[] str)
-        {
-            writefln("%s", str);
-            return s_c;
-        }
-        void newline()
-        {
-            
-        }
-    }
+	public import std.stdio;
+	
+	//simple abstraction of Cout & Cin for phobos	
+	class Cout
+	{
+		static Cout s_c;
+		static this()
+		{
+			s_c = new Cout();
+		}
+		
+		static Cout opCall(char[] str)
+		{
+			writefln("%s", str);
+			return s_c;
+		}
+		void newline()
+		{
+			
+		}
+	}
 
-    class Cin
-    {
-        static char[] get()
-        {
-            return readln();
-        }
-    }
+	class Cin
+	{
+		static char[] get()
+		{
+			return readln();
+		}
+	}
 
-    public import std.string : atoi;
-    alias atoi parse;
+	public import std.string : atoi;
+	alias atoi parse;
 }
