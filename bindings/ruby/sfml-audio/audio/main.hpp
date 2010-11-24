7c2b4bccfa22a7ed68ee5a6cbdac25fb8edc8fda
groogy <groogy@4e206d99-4929-0410-ac5d-dfc041789085>
2010-11-24 18:52:07 +0000

    Started working on the SFML-Audio module. Thought I committed this previously but doesn't look like it. Anyway here it is. Completed at least Listener
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/branches/sfml2@1703 4e206d99-4929-0410-ac5d-dfc041789085

---
+++ bindings/ruby/sfml-audio/audio/main.hpp
@@ -0,0 +1,37 @@
+/* rbSFML - Copyright (c) 2010 Henrik Valter Vogelius Hansson - groogy@groogy.se
+ * This software is provided 'as-is', without any express or
+ * implied warranty. In no event will the authors be held
+ * liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute
+ * it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented;
+ *    you must not claim that you wrote the original software.
+ *    If you use this software in a product, an acknowledgment
+ *    in the product documentation would be appreciated but
+ *    is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such,
+ *    and must not be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any
+ *    source distribution.
+ */
+ 
+#ifndef SFML_RUBYEXT_AUDIO_MAIN_HEADER_
+#define SFML_RUBYEXT_AUDIO_MAIN_HEADER_
+
+#include "ruby.h"
+#include "../../sfml-system/system/main.hpp"
+
+// Ruby initiation function
+extern "C" void Init_audio( void );
+
+typedef VALUE ( *RubyFunctionPtr )( ... );
+
+#define rb_define_singleton_method( klass, name, func, argc, ... ) rb_define_singleton_method( klass, name, reinterpret_cast< RubyFunctionPtr >( func ), argc, ##__VA_ARGS__ )
+#define rb_define_method( klass, name, func, argc, ... ) rb_define_method( klass, name, reinterpret_cast< RubyFunctionPtr >( func ), argc, ##__VA_ARGS__ )
+
+#endif // SFML_RUBYEXT_AUDIO_MAIN_HEADER_
