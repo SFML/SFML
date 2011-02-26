0a2abc6933c38362f0d0b1293fa1bca2b2894971
groogy <groogy@4e206d99-4929-0410-ac5d-dfc041789085>
2011-02-26 01:09:31 +0000

    sfml/all couldn't be loaded since it's symbol was not C style but got name mangled by C++.
    Fixed now :)
    
    git-svn-id: https://sfml.svn.sourceforge.net/svnroot/sfml/branches/sfml2@1801 4e206d99-4929-0410-ac5d-dfc041789085

--- bindings/ruby/sfml-all/all/main.cpp
+++ bindings/ruby/sfml-all/all/main.cpp
@@ -1,9 +1,12 @@
 #include "ruby.h"
 
-void Init_all( void )
+extern "C"
 {
-	rb_require( "sfml/system" );
-	rb_require( "sfml/window" );
-	rb_require( "sfml/graphics" );
-	rb_require( "sfml/audio" );
+	void Init_all( void )
+	{
+		rb_require( "sfml/system" );
+		rb_require( "sfml/window" );
+		rb_require( "sfml/graphics" );
+		rb_require( "sfml/audio" );
+	}
 }
