a198d1e0fa1babef59541716c8bcd9894adfd965
Laurent Gomila <laurent.gom@gmail.com>
2011-07-06 22:34:01 +0200

    Removed the .Net binding (moved to its own repository)

--- bindings/dotnet/build/vc2008/batch-build/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild
-
-echo Compiling release libraries...
-%MSBUILD% ..\SFML.net.sln /verbosity:q /maxcpucount /target:sfml-window,sfml-graphics,sfml-audio /property:Configuration="Release"
-
-echo Done
