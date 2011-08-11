4c3bd58ef6ed93a56fc6568c5e36c33814b75a99
Laurent Gomila <laurent.gom@gmail.com>
2011-08-11 22:24:25 +0200

    Config.hpp doesn't define WIN32_LEAN_AND_MEAN anymore on Windows

--- include/SFML/Config.hpp
+++ include/SFML/Config.hpp
@@ -40,9 +40,6 @@
 
     // Windows
     #define SFML_SYSTEM_WINDOWS
-    #ifndef WIN32_LEAN_AND_MEAN
-        #define WIN32_LEAN_AND_MEAN
-    #endif
     #ifndef NOMINMAX
         #define NOMINMAX
     #endif
