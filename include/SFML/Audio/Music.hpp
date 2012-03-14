ac4efb54f3f0cd649c9797d38504b0330d5baf1e
Laurent Gomila <laurent.gom@gmail.com>
2012-03-14 19:05:51 +0100

    Improved the documentation of Music::openFromXxx functions

--- include/SFML/Audio/Music.hpp
+++ include/SFML/Audio/Music.hpp
@@ -91,6 +91,10 @@ public :
     /// Here is a complete list of all the supported audio formats:
     /// ogg, wav, flac, aiff, au, raw, paf, svx, nist, voc, ircam,
     /// w64, mat4, mat5 pvf, htk, sds, avr, sd2, caf, wve, mpc2k, rf64.
+    /// Since the music is not loaded completely but rather streamed
+    /// continuously, the \a data must remain available as long as the
+    /// music is playing (ie. you can't deallocate it right after calling
+    /// this function).
     ///
     /// \param data        Pointer to the file data in memory
     /// \param sizeInBytes Size of the data to load, in bytes
@@ -110,6 +114,10 @@ public :
     /// Here is a complete list of all the supported audio formats:
     /// ogg, wav, flac, aiff, au, raw, paf, svx, nist, voc, ircam,
     /// w64, mat4, mat5 pvf, htk, sds, avr, sd2, caf, wve, mpc2k, rf64.
+    /// Since the music is not loaded completely but rather streamed
+    /// continuously, the \a stream must remain alive as long as the
+    /// music is playing (ie. you can't destroy it right after calling
+    /// this function).
     ///
     /// \param stream Source stream to read from
     ///
