8327870c9fd4a17b86388cde5186bcc48d53a590
Laurent Gomila <laurent.gom@gmail.com>
2012-05-06 17:32:10 +0200

    Fixed warnings with recent MinGW versions that already define the Windows version macros

--- src/SFML/Window/Win32/InputImpl.cpp
+++ src/SFML/Window/Win32/InputImpl.cpp
@@ -25,6 +25,12 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#ifdef _WIN32_WINDOWS
+    #undef _WIN32_WINDOWS
+#endif
+#ifdef _WIN32_WINNT
+    #undef _WIN32_WINNT
+#endif
 #define _WIN32_WINDOWS 0x0501
 #define _WIN32_WINNT   0x0501
 #include <SFML/Window/Window.hpp>
