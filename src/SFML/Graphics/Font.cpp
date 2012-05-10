5207930169f8b952f484c8c361f5af56dae6fb52
Laurent Gomila <laurent.gom@gmail.com>
2012-05-10 18:07:17 +0200

    Fixed glyphs sometimes not shown with high font sizes

--- src/SFML/Graphics/Font.cpp
+++ src/SFML/Graphics/Font.cpp
@@ -535,7 +535,7 @@ IntRect Font::findGlyphRect(Page& page, unsigned int width, unsigned int height)
     if (!row)
     {
         int rowHeight = height + height / 10;
-        if (page.nextRow + rowHeight >= page.texture.getSize().y)
+        while (page.nextRow + rowHeight >= page.texture.getSize().y)
         {
             // Not enough space: resize the texture if possible
             unsigned int textureWidth  = page.texture.getSize().x;
