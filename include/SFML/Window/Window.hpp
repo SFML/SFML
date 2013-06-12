da96ec5811d67c6692751c27b5dc905a23dce3ed
Laurent Gomila <laurent.gom@gmail.com>
2013-06-12 20:27:18 +0200

    Improved the performances of Window::getSize() (the size is now cached)

--- include/SFML/Window/Window.hpp
+++ include/SFML/Window/Window.hpp
@@ -473,6 +473,7 @@ private:
     priv::GlContext*  m_context;        ///< Platform-specific implementation of the OpenGL context
     Clock             m_clock;          ///< Clock for measuring the elapsed time between frames
     Time              m_frameTimeLimit; ///< Current framerate limit
+    Vector2u          m_size;           ///< Current size of the window
 };
 
 } // namespace sf
