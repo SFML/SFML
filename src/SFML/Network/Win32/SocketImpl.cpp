76b67fcd1cde69f3d8d652f7660dc752232de104
Laurent Gomila <laurent.gom@gmail.com>
2013-06-17 23:12:44 +0200

    Fixed non-blocking connection with a sf::TcpSocket on Windows

--- src/SFML/Network/Win32/SocketImpl.cpp
+++ src/SFML/Network/Win32/SocketImpl.cpp
@@ -74,11 +74,13 @@ Socket::Status SocketImpl::getErrorStatus()
     switch (WSAGetLastError())
     {
         case WSAEWOULDBLOCK :  return Socket::NotReady;
+        case WSAEALREADY :     return Socket::NotReady;
         case WSAECONNABORTED : return Socket::Disconnected;
         case WSAECONNRESET :   return Socket::Disconnected;
         case WSAETIMEDOUT :    return Socket::Disconnected;
         case WSAENETRESET :    return Socket::Disconnected;
         case WSAENOTCONN :     return Socket::Disconnected;
+        case WSAEISCONN :      return Socket::Done; // when connecting a non-blocking socket
         default :              return Socket::Error;
     }
 }
