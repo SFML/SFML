47e255fcad2781f340f2c90256b76110f53a0d0c
Colin GILLE <congelli501@gmail.com>
2013-07-07 19:48:17 +0200

    Add FPS and flip fullscreen / windowed mode in the Fullscreen example

--- examples/fullscreen/Fullscreen.cpp
+++ examples/fullscreen/Fullscreen.cpp
@@ -4,6 +4,7 @@
 ////////////////////////////////////////////////////////////
 #include <SFML/Graphics.hpp>
 #include <algorithm>
+#include <sstream>
 
 
 ////////////////////////////////////////////////////////////
@@ -12,18 +13,48 @@
 /// \return Application exit code
 ///
 ////////////////////////////////////////////////////////////
+sf::RenderWindow *createRenderWindw(bool isFullscreen, sf::VideoMode mode, const sf::Image &icon)
+{
+	long flag = sf::Style::Fullscreen;
+	
+	if(!isFullscreen) // Not in full screen -> window is half the size of the screen
+	{
+		mode.width /= 2;
+		mode.height /= 2;
+		
+		flag = sf::Style::Default;
+	}
+	
+    sf::RenderWindow *window = new sf::RenderWindow(mode, "SFML Fullscreen", flag);
+    window->setVerticalSyncEnabled(false);
+    window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    window->setTitle("AAAAA");
+    
+    return window;
+}
+
 int main()
 {
 	// Desktop size
 	sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+	bool isFullscreen = true;
+	
+    // Icon & image
+    sf::Image image;
+    if(! image.loadFromFile("resources/icon.png"))
+		return EXIT_FAILURE;
+
+    // Load the text font
+    sf::Font font;
+    if (!font.loadFromFile("resources/sansation.ttf"))
+        return EXIT_FAILURE;
+	
+    // Create the window of the application
+    sf::RenderWindow *window = createRenderWindw(isFullscreen, mode, image);
 
     // Define some constants
     const float ballRadius = 20.f;
     bool hasFocus = true;
-
-    // Create the window of the application
-    sf::RenderWindow window(mode, "SFML Fullscreen", sf::Style::Fullscreen | sf::Style::FullscreenWM);
-    window.setVerticalSyncEnabled(true);
 
     // Create the key ball
     sf::CircleShape keyBall;
@@ -32,7 +63,7 @@ int main()
     keyBall.setOutlineColor(sf::Color::Black);
     keyBall.setFillColor(sf::Color::Blue);
     keyBall.setOrigin(ballRadius / 2.f, ballRadius / 2.f);
-    keyBall.setPosition(mode.width / 2, mode.height / 2);
+    keyBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
     
     // Create the mouse ball
     sf::CircleShape mouseBall;
@@ -41,37 +72,44 @@ int main()
     mouseBall.setOutlineColor(sf::Color::Black);
     mouseBall.setFillColor(sf::Color::Red);
     mouseBall.setOrigin(ballRadius / 2.f, ballRadius / 2.f);
-    mouseBall.setPosition(mode.width / 2, mode.height / 2);
-
-    // Load the text font
-    sf::Font font;
-    if (!font.loadFromFile("resources/sansation.ttf"))
-        return EXIT_FAILURE;
+    mouseBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
 
     // Initialize the pause message
     sf::Text msg;
     msg.setFont(font);
-    msg.setCharacterSize(30);
+    msg.setCharacterSize(15);
     msg.setPosition(20.f, 100.f);
     msg.setColor(sf::Color::White);
-    msg.setString("This example is used to show the new Window Manager based fullscreen mode\nAs you can see, keyboard events works (you can move the ball using the direction keys).\nThe mouse events work too.\n\nYou can also alt-tab the application, change the volume using your hot / special keys,\n   close the window using ALT+F4, switch desktop...");
+    msg.setString("This example is used to show the new Window Manager based fullscreen mode\nAs you can see, keyboard events works (you can move the ball using the direction keys).\nThe mouse events work too.\n\nYou can also alt-tab the application, change the volume using your hot / special keys,\n   close the window using ALT+F4, switch desktop...\n\nPress <Escape> to quit or <Tab> to switch to fullscreen or windowed mode.");
+    
+    // FPS
+    sf::Text fpsText;
+    fpsText.setFont(font);
+    fpsText.setCharacterSize(20);
+    fpsText.setPosition(5.f, 5.f);
+    fpsText.setColor(sf::Color::White);
+    fpsText.setString("...");
 
     // Define the paddles properties
     sf::Clock AITimer;
     const float speed   = 200.f;
+    
+    // FPS
+    int frameCount = 0;
+    float time = 0.f;
 
     sf::Clock clock;
-    while (window.isOpen())
+    while (window->isOpen())
     {
         // Handle events
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window->pollEvent(event))
         {
             // Window closed or escape key pressed: exit
             if ((event.type == sf::Event::Closed) || 
                ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
             {
-                window.close();
+                window->close();
                 break;
             }
             else if(event.type == sf::Event::GainedFocus)
@@ -82,13 +120,39 @@ int main()
 			{
 				hasFocus = false;
 			}
+			else if((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Tab))
+			{
+				isFullscreen = !isFullscreen;
+				
+				delete window;
+				window = createRenderWindw(isFullscreen, mode, image);
+				
+				// Move objects
+				keyBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
+				mouseBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
+			}
         }
+
+		// Frame time
+		float deltaTime = clock.restart().asSeconds();
+		
+		// FPS - We calculate the current frame rate every 2 seconds to have readable results
+		frameCount++;
+		time += deltaTime;
+		
+		if(time > 2.f)
+		{
+			std::stringstream strstrm;
+			strstrm << frameCount / time;
+			fpsText.setString(strstrm.str());
+			
+			time = 0;
+			frameCount = 0;
+		}
 
 		// Key ball
 		if(hasFocus)
 		{
-			float deltaTime = clock.restart().asSeconds();
-
 			int xDirection = 0;
 			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 				xDirection = 1;
@@ -105,29 +169,32 @@ int main()
 			
 			sf::Vector2f pos = keyBall.getPosition();
 			pos.x = std::max(pos.x, 0.f);
-			pos.x = std::min(pos.x, float(mode.width));
+			pos.x = std::min(pos.x, float(window->getSize().x));
 			
 			pos.y = std::max(pos.y, 0.f);
-			pos.y = std::min(pos.y, float(mode.height));
+			pos.y = std::min(pos.y, float(window->getSize().y));
 			
 			keyBall.setPosition(pos);
 			
 			// Mouse ball
-			sf::Vector2i mousePos = sf::Mouse::getPosition();
+			sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
 			mouseBall.setPosition(mousePos.x, mousePos.y);
 		}
 
         // Clear the window
-        window.clear(sf::Color(50, 200, 50));
-
+        window->clear(sf::Color(50, 200, 50));
+        
 		// Draw the paddles and the ball
-		window.draw(keyBall);
-		window.draw(mouseBall);
-		window.draw(msg);
+		window->draw(keyBall);
+		window->draw(mouseBall);
+		window->draw(msg);
+		window->draw(fpsText);
 
         // Display things on screen
-        window.display();
+        window->display();
     }
+    
+    delete window;
 
     return EXIT_SUCCESS;
 }
