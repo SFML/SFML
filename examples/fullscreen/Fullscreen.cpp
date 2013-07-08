0499fce4d0f659b90bc37ac6f66445085623ab01
Colin GILLE <congelli501@gmail.com>
2013-07-08 11:31:50 +0200

    Replace tabs by 4 spaces - should fix the indentation issue

--- examples/fullscreen/Fullscreen.cpp
+++ examples/fullscreen/Fullscreen.cpp
@@ -15,16 +15,16 @@
 ////////////////////////////////////////////////////////////
 sf::RenderWindow *createRenderWindw(bool isFullscreen, sf::VideoMode mode, const sf::Image &icon)
 {
-	long flag = sf::Style::Fullscreen;
-	
-	if(!isFullscreen) // Not in full screen -> window is half the size of the screen
-	{
-		mode.width /= 2;
-		mode.height /= 2;
-		
-		flag = sf::Style::Default;
-	}
-	
+    long flag = sf::Style::Fullscreen;
+    
+    if(!isFullscreen) // Not in full screen -> window is half the size of the screen
+    {
+        mode.width /= 2;
+        mode.height /= 2;
+        
+        flag = sf::Style::Default;
+    }
+    
     sf::RenderWindow *window = new sf::RenderWindow(mode, "SFML Fullscreen", flag);
     window->setVerticalSyncEnabled(false);
     window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
@@ -34,20 +34,20 @@ sf::RenderWindow *createRenderWindw(bool isFullscreen, sf::VideoMode mode, const
 
 int main()
 {
-	// Desktop size
-	sf::VideoMode mode = sf::VideoMode::getDesktopMode();
-	bool isFullscreen = true;
-	
+    // Desktop size
+    sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+    bool isFullscreen = true;
+    
     // Icon & image
     sf::Image image;
     if(! image.loadFromFile("resources/icon.png"))
-		return EXIT_FAILURE;
+        return EXIT_FAILURE;
 
     // Load the text font
     sf::Font font;
     if (!font.loadFromFile("resources/sansation.ttf"))
         return EXIT_FAILURE;
-	
+    
     // Create the window of the application
     sf::RenderWindow *window = createRenderWindw(isFullscreen, mode, image);
 
@@ -113,81 +113,81 @@ int main()
             }
             else if(event.type == sf::Event::GainedFocus)
             {
-				hasFocus = true;
-			}
-			else if(event.type == sf::Event::LostFocus)
-			{
-				hasFocus = false;
-			}
-			else if((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Tab))
-			{
-				isFullscreen = !isFullscreen;
-				
-				delete window;
-				window = createRenderWindw(isFullscreen, mode, image);
-				
-				// Move objects
-				keyBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
-				mouseBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
-			}
+                hasFocus = true;
+            }
+            else if(event.type == sf::Event::LostFocus)
+            {
+                hasFocus = false;
+            }
+            else if((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Tab))
+            {
+                isFullscreen = !isFullscreen;
+                
+                delete window;
+                window = createRenderWindw(isFullscreen, mode, image);
+                
+                // Move objects
+                keyBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
+                mouseBall.setPosition(sf::Vector2f(window->getSize()) / 2.f);
+            }
         }
 
-		// Frame time
-		float deltaTime = clock.restart().asSeconds();
-		
-		// FPS - We calculate the current frame rate every 2 seconds to have readable results
-		frameCount++;
-		time += deltaTime;
-		
-		if(time > 2.f)
-		{
-			std::stringstream strstrm;
-			strstrm << frameCount / time;
-			fpsText.setString(strstrm.str());
-			
-			time = 0;
-			frameCount = 0;
-		}
+        // Frame time
+        float deltaTime = clock.restart().asSeconds();
+        
+        // FPS - We calculate the current frame rate every 2 seconds to have readable results
+        frameCount++;
+        time += deltaTime;
+        
+        if(time > 2.f)
+        {
+            std::stringstream strstrm;
+            strstrm << frameCount / time;
+            fpsText.setString(strstrm.str());
+            
+            time = 0;
+            frameCount = 0;
+        }
 
-		// Key ball
-		if(hasFocus)
-		{
-			int xDirection = 0;
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-				xDirection = 1;
-			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				xDirection = -1;
-			
-			int yDirection = 0;
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-				yDirection = 1;
-			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-				yDirection = -1;
-			
-			keyBall.move(speed * deltaTime * xDirection, speed * deltaTime * yDirection);
-			
-			sf::Vector2f pos = keyBall.getPosition();
-			pos.x = std::max(pos.x, 0.f);
-			pos.x = std::min(pos.x, float(window->getSize().x));
-			
-			pos.y = std::max(pos.y, 0.f);
-			pos.y = std::min(pos.y, float(window->getSize().y));
-			
-			keyBall.setPosition(pos);
-			
-			// Mouse ball
-			sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
-			mouseBall.setPosition(mousePos.x, mousePos.y);
-		}
+        // Key ball
+        if(hasFocus)
+        {
+            int xDirection = 0;
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                xDirection = 1;
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                xDirection = -1;
+            
+            int yDirection = 0;
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                yDirection = 1;
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                yDirection = -1;
+            
+            keyBall.move(speed * deltaTime * xDirection, speed * deltaTime * yDirection);
+            
+            sf::Vector2f pos = keyBall.getPosition();
+            pos.x = std::max(pos.x, 0.f);
+            pos.x = std::min(pos.x, float(window->getSize().x));
+            
+            pos.y = std::max(pos.y, 0.f);
+            pos.y = std::min(pos.y, float(window->getSize().y));
+            
+            keyBall.setPosition(pos);
+            
+            // Mouse ball
+            sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
+            mouseBall.setPosition(mousePos.x, mousePos.y);
+        }
 
         // Clear the window
         window->clear(sf::Color(50, 200, 50));
         
-		// Draw the paddles and the ball
-		window->draw(keyBall);
-		window->draw(mouseBall);
-		window->draw(msg);
-		window->draw(fpsText);
+        // Draw the paddles and the ball
+        window->draw(keyBall);
+        window->draw(mouseBall);
+        window->draw(msg);
+        window->draw(fpsText);
 
         // Display things on screen
         window->display();
