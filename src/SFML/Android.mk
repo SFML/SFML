8f72fc07fd1e5dd3a08e2502c1ff654af775d63e
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-09-23 13:39:08 +0200

    [Android] Removed ogg, FLAC, vorbis and vorbisenc dependencies

--- src/SFML/Android.mk
+++ src/SFML/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sfml-system
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-system.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_EXPORT_LDLIBS := -llog -landroid
 
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -12,7 +11,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sfml-window
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-window.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_EXPORT_LDLIBS := -lEGL
+LOCAL_EXPORT_LDLIBS := -lEGL -llog -landroid
 LOCAL_SHARED_LIBRARIES := sfml-system
 
 include $(PREBUILT_SHARED_LIBRARY)
@@ -21,10 +20,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sfml-graphics
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-graphics.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-#LOCAL_EXPORT_LDLIBS := -lGLESv1_CM
-
-LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/extlibs/include
-LOCAL_SHARED_LIBRARIES := freetype jpeg
 LOCAL_SHARED_LIBRARIES += sfml-system sfml-window
 
 include $(PREBUILT_SHARED_LIBRARY)
@@ -33,9 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sfml-audio
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-audio.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-#LOCAL_EXPORT_LDLIBS := -lOpenSLES
-LOCAL_SHARED_LIBRARIES := openal sndfile
-LOCAL_SHARED_LIBRARIES += sfml-system sfml-window
+LOCAL_SHARED_LIBRARIES := sfml-window sfml-system openal sndfile
 
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -51,7 +44,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sfml-main
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-main.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_SHARED_LIBRARIES := sfml-system sfml-window
+LOCAL_SHARED_LIBRARIES := sfml-window sfml-system
 
 include $(PREBUILT_STATIC_LIBRARY)
 
