de162eaa3ef251f28fb2803ff467bb904002ebb7
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2014-03-11 16:03:04 +0100

    [Android] Updated scripts to use libc++ when building extlibs

--- tools/android/compile_arm-v7a.sh
+++ tools/android/compile_arm-v7a.sh
@@ -7,6 +7,6 @@ CC=arm-linux-androideabi-gcc
 CXX=arm-linux-androideabi-g++
 CFLAGS="-I$DESTDIR/usr/include -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
 CPPFLAGS="-I$DESTDIR/usr/include -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
-LDFLAGS="-L$DESTDIR/usr/lib -march=armv7-a -Wl,--fix-cortex-a8 -lstlport_shared"
+LDFLAGS="-L$DESTDIR/usr/lib -march=armv7-a -Wl,--fix-cortex-a8"
 
 ./compile_libs.sh arm-v7a $PATH $CC $CXX "$CFLAGS" "$CPPFLAGS" "$LDFLAGS"
