af4dac6fedb85a23364d711a98b44ff9cc445d6c
Mario Liebisch <mario.liebisch@gmail.com>
2015-02-25 10:46:37 +0100

    Marked the FLAC/Vorbis/Ogg variables as advanced

--- cmake/Modules/FindFLAC.cmake
+++ cmake/Modules/FindFLAC.cmake
@@ -14,3 +14,5 @@ find_library(FLAC_LIBRARY NAMES FLAC)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FLAC DEFAULT_MSG FLAC_LIBRARY FLAC_INCLUDE_DIR)
+
+mark_as_advanced(FLAC_INCLUDE_DIR FLAC_LIBRARY)
