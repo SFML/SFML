0bcd7d39f263a76103cee2a186882114bc5324af
Jan Haller <bromeon@gmail.com>
2015-06-25 21:38:49 +0200

    Made implementation of glCheck and alCheck macros more robust
    At the moment, glCheck(...) and alCheck(...) look like a function calls, which is misleading and can cause subtle bugs, for example when used in if statements. This change mitigates the situation by allowing those expressions to be used as single statements within if/else branches.
    
    Initializations of OpenGL handles that previously involved glCheck() calls now need to be split into separate declaration and assignment.

--- src/SFML/Audio/ALCheck.hpp
+++ src/SFML/Audio/ALCheck.hpp
@@ -50,7 +50,8 @@ namespace priv
 #ifdef SFML_DEBUG
 
     // If in debug mode, perform a test on every call
-    #define alCheck(x) x; sf::priv::alCheckError(__FILE__, __LINE__);
+    // The do-while loop is needed so that alCheck can be used as a single statement in if/else branches
+    #define alCheck(expr) do { expr; sf::priv::alCheckError(__FILE__, __LINE__, #expr); } while (false)
 
 #else
 
