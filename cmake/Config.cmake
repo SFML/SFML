e199ef3790dd1ab620f5d55ec20ea885b018fe0c
James Cowgill <james410@cowgill.org.uk>
2016-08-11 21:46:26 +0100

    Define SFML_OS_FREEBSD when compiling for kFreeBSD

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -27,7 +27,7 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
         # don't use the OpenGL ES implementation on Linux
         set(OPENGL_ES 0)
     endif()
-elseif(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
+elseif(CMAKE_SYSTEM_NAME MATCHES "^k?FreeBSD$")
     set(SFML_OS_FREEBSD 1)
     # don't use the OpenGL ES implementation on FreeBSD
     set(OPENGL_ES 0)
