8a02dc0ad59ff5ab7c6cb69a0eb50c2e0e3c005b
marukrap <marukrap@gmail.com>
2017-10-24 07:06:29 +0900

    Change a character
    from en-dash(u2013) to hypen-minus(u002d)

--- include/SFML/Window/WindowHandle.hpp
+++ include/SFML/Window/WindowHandle.hpp
@@ -65,7 +65,7 @@ namespace sf
 #elif defined(SFML_DOXYGEN)
 
     // Define typedef symbol so that Doxygen can attach some documentation to it
-    typedef "platform–specific" WindowHandle;
+    typedef "platform-specific" WindowHandle;
 
 #endif
 
