5d7843c58a850b4941d7d9794d6fede2bbc02833
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2018-01-29 19:03:53 +0100

    Updated the copyright year to 2018.

--- license.md
+++ license.md
@@ -1,6 +1,6 @@
 # SFML
 
-SFML - Copyright (C) 2007-2017 Laurent Gomila - laurent@sfml-dev.org
+SFML - Copyright (C) 2007-2018 Laurent Gomila - laurent@sfml-dev.org
 
 This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
 
--- src/SFML/Main/MainiOS.mm
+++ src/SFML/Main/MainiOS.mm
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2017 Laurent Gomila (laurent@sfml-dev.prg)
+// Copyright (C) 2007-2018 Laurent Gomila (laurent@sfml-dev.prg)
 // Copyright (C) 2013 Jonathan De Wachter (dewachter.jonathan@gmail.com)
 //
 // This software is provided 'as-is', without any express or implied warranty.
