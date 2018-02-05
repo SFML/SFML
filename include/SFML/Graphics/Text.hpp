deeb3a95f3ff46cc350cbabb08802cf6fb9ddac6
Radek Dutkiewicz <oomek@users.noreply.github.com>
2018-02-05 15:14:08 +0000

    Fixed letter spacing being letter width dependent

--- include/SFML/Graphics/Text.hpp
+++ include/SFML/Graphics/Text.hpp
@@ -149,9 +149,9 @@ public:
     ///
     /// The default spacing between lines is defined by the font.
     /// This method enables you to set a factor for the spacing
-    /// between lines. By default the line spacing offset is 1.
+    /// between lines. By default the line spacing factor is 1.
     ///
-    /// \param spacing New line spacing factor
+    /// \param spacingFactor New line spacing factor
     ///
     /// \see getLineSpacing
     ///
@@ -162,10 +162,15 @@ public:
     /// \brief Set the letter spacing factor
     ///
     /// The default spacing between letters is defined by the font.
-    /// This method enables you to set a factor to the spacing
-    /// between letters. By default the letter spacing factor is 1.
+    /// This factor doesn't directly apply to the existing
+    /// spacing between each character, it rather adds a fixed
+    /// space between them which is calculated from the font
+    /// metrics and the character size.
+    /// Note that factors below 1 (including negative numbers) bring
+    /// characters closer to each other.
+    /// By default the letter spacing factor is 1.
     ///
-    /// \param spacing New letter spacing factor
+    /// \param spacingFactor New letter spacing factor
     ///
     /// \see getLetterSpacing
     ///
