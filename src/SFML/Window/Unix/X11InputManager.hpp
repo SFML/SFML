fa16402a93ee3c99026732b1926c02e877794e04
Elias Daler <eliasdaler@yandex.ru>
2018-02-21 01:05:39 +0300

    Initial scancode implementation for Linux

---
+++ src/SFML/Window/Unix/X11InputManager.hpp
@@ -0,0 +1,45 @@
+#ifndef SFML_X11INPUTMANAGER_HPP
+#define SFML_X11INPUTMANAGER_HPP
+
+#include <SFML/System/String.hpp>
+#include <SFML/Window/Keyboard.hpp>
+
+#include <X11/X.h> // Keycode
+#include <X11/XKBlib.h>
+
+namespace sf {
+namespace priv {
+
+class X11InputManager
+{
+public:
+    static X11InputManager& getInstance();
+private:
+    X11InputManager();
+public:
+    void initialize();
+
+    sf::Keyboard::Scancode unlocalize(sf::Keyboard::Key key) const;
+    sf::Keyboard::Key localize(sf::Keyboard::Scancode code) const;
+
+    bool isKeyPressed(sf::Keyboard::Key key) const;
+    bool isKeyPressed(sf::Keyboard::Scancode code) const;
+
+    sf::Keyboard::Key getKeyFromEvent(XKeyEvent& event) const;
+    sf::Keyboard::Scancode getScancodeFromEvent(XKeyEvent& event) const;
+
+    String getDescription(Keyboard::Scancode code) const;
+private:
+    void buildMapping();
+    KeyCode SFtoKeyCode(Display* display, sf::Keyboard::Key key) const;
+    KeyCode SFtoKeyCode(Display* display, sf::Keyboard::Scancode code) const;
+    sf::Keyboard::Scancode keyCodeToSF(KeyCode code) const;
+
+    KeyCode m_scancodeToKeycode[sf::Keyboard::ScanCodeCount];
+    sf::Keyboard::Scancode m_keycodeToScancode[256];
+};
+
+} // end of namespace priv
+} // end of namespace sf
+
+#endif
