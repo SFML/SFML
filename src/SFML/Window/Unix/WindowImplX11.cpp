8626b0470503934d5fa9889654b5a06fb5c62736
Elias Daler <eliasdaler@yandex.ru>
2018-03-29 21:11:37 +0300

    Store pointer to Display in X11InputManager

--- src/SFML/Window/Unix/WindowImplX11.cpp
+++ src/SFML/Window/Unix/WindowImplX11.cpp
@@ -1688,6 +1688,9 @@ void WindowImplX11::initialize()
     // Create the hidden cursor
     createHiddenCursor();
 
+    // init X11 keycode <-> SFML scancode mapping
+    X11InputManager::getInstance().initialize(m_display);
+
     // Flush the commands queue
     XFlush(m_display);
 
@@ -2281,5 +2284,4 @@ Vector2i WindowImplX11::getPrimaryMonitorPosition()
 }
 
 } // namespace priv
-
 } // namespace sf
