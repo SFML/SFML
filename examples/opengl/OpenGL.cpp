e8be39d15aea98c5f18a1adedb3f9bdb2390fdcb
Ceylo <lucas.soltic@orange.fr>
2018-04-11 21:45:36 +0200

    All mobile-compatible examples now successfully link

--- examples/opengl/OpenGL.cpp
+++ examples/opengl/OpenGL.cpp
@@ -5,6 +5,15 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 
+#ifdef SFML_SYSTEM_IOS
+#include <SFML/Main.hpp>
+#endif
+
+#ifdef SFML_OPENGL_ES
+#define glClearDepth glClearDepthf
+#define glFrustum glFrustumf
+#endif
+
 #ifndef GL_SRGB8_ALPHA8
 #define GL_SRGB8_ALPHA8 0x8C43
 #endif
