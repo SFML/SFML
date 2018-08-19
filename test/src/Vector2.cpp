9284c74accdd6dc162b613a3eae7551f9a926a61
Jonny Paton <jonathan.r.paton@googlemail.com>
2018-08-19 19:04:11 +0100

    Use modern cmake testing functionality including catch integration

--- test/src/Vector2.cpp
+++ test/src/Vector2.cpp
@@ -1,3 +1,5 @@
+#define CATCH_CONFIG_MAIN
+
 #include <SFML/System/Vector2.hpp>
 #include "TestUtilities/System.hpp"
 
---
+++ test/src/Window.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/System/Vector2.hpp>
+#include "TestUtilities/Window.hpp"
+
+TEST_CASE("Window unit test exists", "[window]")
+{
+    CHECK(TRUE);
+}
