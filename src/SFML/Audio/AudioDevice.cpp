79729747677452e70bc828eeb0496682b59200ec
David Demelier <markand@malikania.fr>
2018-11-07 13:27:42 +0100

    Use C++11 std::unique_ptr
    The std::auto_ptr class will be removed in C++17 and has been deprecated
    since C++11.

--- src/SFML/Audio/AudioDevice.cpp
+++ src/SFML/Audio/AudioDevice.cpp
@@ -107,7 +107,7 @@ bool AudioDevice::isExtensionSupported(const std::string& extension)
     // This device will not be used in this function and merely
     // makes sure there is a valid OpenAL device for extension
     // queries if none has been created yet.
-    std::auto_ptr<AudioDevice> device;
+    std::unique_ptr<AudioDevice> device;
     if (!audioDevice)
         device.reset(new AudioDevice);
 
@@ -125,7 +125,7 @@ int AudioDevice::getFormatFromChannelCount(unsigned int channelCount)
     // This device will not be used in this function and merely
     // makes sure there is a valid OpenAL device for format
     // queries if none has been created yet.
-    std::auto_ptr<AudioDevice> device;
+    std::unique_ptr<AudioDevice> device;
     if (!audioDevice)
         device.reset(new AudioDevice);
 
