5449c3f3696e90a9e5a9d9b1108783227a612788
Austin Krauss <austin.krauss@gmail.com>
2019-01-12 22:22:31 -0800

    Fix compiler warning

--- src/SFML/Window/Android/WindowImplAndroid.cpp
+++ src/SFML/Window/Android/WindowImplAndroid.cpp
@@ -666,6 +666,8 @@ Keyboard::Key WindowImplAndroid::androidKeyToSF(int32_t key)
         case AKEYCODE_BUTTON_SELECT:
         case AKEYCODE_BUTTON_MODE:        return Keyboard::Unknown;
     }
+
+    return Keyboard::Unknown;
 }
 
 
