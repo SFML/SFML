4fab55bf61d0179334beee9fc7534e1a9d9d7a0a
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2020-11-18 20:16:37 +0100

    Update CMake integration for Opus

--- cmake/Modules/FindOpus.cmake
+++ cmake/Modules/FindOpus.cmake
@@ -1,15 +1,9 @@
-# This file was taken from Unvanquished,
-# Copyright 2000-2009 Kitware, Inc., Insight Software Consortium
-# It's licensed under the terms of the 3-clause OpenBSD license.
-# Modifications Copyright 2014-2015 the openage authors.
-# See copying.md for further legal info.
-
-# - Find opus library
-# Find the native Opus headers and libraries.
-# This module defines
-#  OPUS_INCLUDE_DIRS   - where to find opus/opus.h, opus/opusfile.h, etc
-#  OPUS_LIBRARIES      - List of libraries when using libopus
-#  OPUS_FOUND          - True if opus is found.
+#
+# Try to find the Opus libraries and include paths
+# OPUS_FOUND
+# OPUS_INCLUDE_DIR
+# OPUS_LIBRARY
+#
 
 # find the opusfile header, defines our api.
 find_path(OPUS_INCLUDE_DIR
@@ -32,12 +26,10 @@ find_library(OPUS_LIBRARY
 )
 mark_as_advanced(OPUS_LIBRARY)
 
-
-# handle the QUIETLY and REQUIRED arguments and set OPUSFILE_FOUND to TRUE if
-# all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Opus DEFAULT_MSG OPUSFILE_LIBRARY OPUS_LIBRARY OPUS_INCLUDE_DIR)
-
-# export the variables
-set(OPUS_LIBRARIES "${OPUSFILE_LIBRARY}" "${OPUS_LIBRARY}")
-set(OPUS_INCLUDE_DIRS "${OPUS_INCLUDE_DIR}" "${OPUS_INCLUDE_DIR}/opus")
+add_library(Opus::opus IMPORTED UNKNOWN)
+set_target_properties(Opus::opus PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OPUS_INCLUDE_DIR})
+if(OPUS_LIBRARY MATCHES "/([^/]+)\\.framework$")
+    set_target_properties(Opus::opus PROPERTIES IMPORTED_LOCATION ${OPUS_LIBRARY}/${CMAKE_MATCH_1})
+else()
+    set_target_properties(Opus::opus PROPERTIES IMPORTED_LOCATION ${OPUS_LIBRARY})
+endif()
