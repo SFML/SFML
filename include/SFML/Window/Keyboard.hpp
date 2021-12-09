756a399909a913364de00742afce6b91441646b9
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-12-09 00:48:24 +0000

    Remove deprecated 'Keyboard::Key' enumerators and adjust usages

--- include/SFML/Window/Keyboard.hpp
+++ include/SFML/Window/Keyboard.hpp
@@ -150,15 +150,7 @@ public:
         F15,          //!< The F15 key
         Pause,        //!< The Pause key
 
-        KeyCount,     //!< Keep last -- the total number of keyboard keys
-
-        // Deprecated values:
-
-        Dash      = Hyphen,       //!< \deprecated Use Hyphen instead
-        BackSpace = Backspace,    //!< \deprecated Use Backspace instead
-        BackSlash = Backslash,    //!< \deprecated Use Backslash instead
-        SemiColon = Semicolon,    //!< \deprecated Use Semicolon instead
-        Return    = Enter         //!< \deprecated Use Enter instead
+        KeyCount      //!< Keep last -- the total number of keyboard keys
     };
 
     ////////////////////////////////////////////////////////////
