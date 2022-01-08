692954b0b11a9458887ee720d4a05a1339aa7ed4
Vittorio Romeo <vittorio.romeo@outlook.com>
2022-01-08 23:31:37 +0100

    Restore clock changes but not sleep changes

--- src/SFML/System/Android/SuspendAwareClock.cpp
+++ src/SFML/System/Android/SuspendAwareClock.cpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2021 Laurent Gomila (laurent@sfml-dev.org)
+// Copyright (C) 2007-2022 Laurent Gomila (laurent@sfml-dev.org)
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -22,40 +22,24 @@
 //
 ////////////////////////////////////////////////////////////
 
-#ifndef SFML_CLOCKIMPLUNIX_HPP
-#define SFML_CLOCKIMPLUNIX_HPP
-
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <SFML/Config.hpp>
-#include <SFML/System/Time.hpp>
-
+#include <SFML/System/SuspendAwareClock.hpp>
+#include <ctime>
 
 namespace sf
 {
-namespace priv
-{
-////////////////////////////////////////////////////////////
-/// \brief Unix implementation of sf::Clock
-///
-////////////////////////////////////////////////////////////
-class ClockImpl
-{
-public:
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Get the current time
-    ///
-    /// \return Current time
-    ///
-    ////////////////////////////////////////////////////////////
-    static Time getCurrentTime();
-};
-
-} // namespace priv
+SuspendAwareClock::time_point SuspendAwareClock::now() noexcept
+{
+    ::timespec ts;
+#ifdef CLOCK_BOOTTIME
+    clock_gettime(CLOCK_BOOTTIME, &ts);
+#else
+    #error "CLOCK_BOOTTIME is essential for SuspendAwareClock to work"
+#endif // CLOCK_BOOTTIME
+    return time_point(std::chrono::seconds(ts.tv_sec) + std::chrono::nanoseconds(ts.tv_nsec));
+}
 
 } // namespace sf
-
-
-#endif // SFML_CLOCKIMPLUNIX_HPP
