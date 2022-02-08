f8c3a56898278623f17ad47cf36b180132a17243
fallahn <matty_styles@hotmail.com>
2022-02-08 22:21:42 +0000

    change reverb parameter names from unsigned to signed

--- src/SFML/Audio/ReverbEffect.cpp
+++ src/SFML/Audio/ReverbEffect.cpp
@@ -40,15 +40,15 @@ namespace
 {
     //these are defined so that they can be shared between EAX/non-EAX
     //versions of the reverb effect. See applyParameterNames()
-    sf::Uint32 REVERB_DENSITY              = 0;
-    sf::Uint32 REVERB_DIFFUSION            = 0;
-    sf::Uint32 REVERB_GAIN                 = 0;
-    sf::Uint32 REVERB_DECAY_TIME           = 0;
-    sf::Uint32 REVERB_REFLECTIONS_GAIN     = 0;
-    sf::Uint32 REVERB_REFLECTIONS_DELAY    = 0;
-    sf::Uint32 REVERB_LATE_REVERB_GAIN     = 0;
-    sf::Uint32 REVERB_LATE_REVERB_DELAY    = 0;
-    sf::Uint32 REVERB_ROOM_ROLLOFF_FACTOR  = 0;
+    sf::Int32 REVERB_DENSITY              = 0;
+    sf::Int32 REVERB_DIFFUSION            = 0;
+    sf::Int32 REVERB_GAIN                 = 0;
+    sf::Int32 REVERB_DECAY_TIME           = 0;
+    sf::Int32 REVERB_REFLECTIONS_GAIN     = 0;
+    sf::Int32 REVERB_REFLECTIONS_DELAY    = 0;
+    sf::Int32 REVERB_LATE_REVERB_GAIN     = 0;
+    sf::Int32 REVERB_LATE_REVERB_DELAY    = 0;
+    sf::Int32 REVERB_ROOM_ROLLOFF_FACTOR  = 0;
 }
 
 namespace sf
