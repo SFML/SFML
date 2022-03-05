25c0c1929a3e354e0ceb8f38351ab1c8f0aac525
Chris Thrasher <chrisjthrasher@gmail.com>
2022-03-04 21:08:56 -0700

    Remove CompileOptionsOverride
    These overrides existed to prevent MSVC errors related to duplicate
    compiler warnings. Because we required a version of CMake older
    3.15, CMake would add /W3 as a default compiler flag when using
    MSVC. We then add /W4 in addition to that. Modern CMake versions
    seem to deduplicate these warnings but older versions did not.
    
    The easist fix is to raise the minimum CMake version to 3.15 which
    changes the default behavior to no longer add /W3 without being
    explicitly specified. See the below link for more information about
    this behavior change.
    
    https://cmake.org/cmake/help/latest/policy/CMP0092.html

--- cmake/CompilerOptionsOverride.cmake
+++
@@ -1,5 +0,0 @@
-if(MSVC)
-	# remove default warning level from CMAKE_CXX_FLAGS_INIT
-	string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
-	string(REGEX REPLACE "/W[0-4]" "" CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT}")
-endif()
