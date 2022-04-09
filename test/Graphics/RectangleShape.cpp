abceba6654ec14647e28b25c5dfa698f499b8815
Chris Thrasher <chrisjthrasher@gmail.com>
2022-04-08 18:33:50 -0600

    Add tests for `sf::RectangleShape`

---
+++ test/Graphics/RectangleShape.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics/RectangleShape.hpp>
+#include "SystemUtil.hpp"
+
+#include <doctest.h>
+
+TEST_CASE("sf::RectangleShape class - [graphics]")
+{
+    SUBCASE("Default constructor")
+    {
+        const sf::RectangleShape rectangle;
+        CHECK(rectangle.getSize() == sf::Vector2f(0, 0));
+        CHECK(rectangle.getPointCount() == 4);
+        CHECK(rectangle.getPoint(0) == sf::Vector2f(0, 0));
+        CHECK(rectangle.getPoint(1) == sf::Vector2f(0, 0));
+        CHECK(rectangle.getPoint(2) == sf::Vector2f(0, 0));
+        CHECK(rectangle.getPoint(3) == sf::Vector2f(0, 0));
+    }
+
+    SUBCASE("Size constructor")
+    {
+        const sf::RectangleShape rectangle({9, 8});
+        CHECK(rectangle.getSize() == sf::Vector2f(9, 8));
+        CHECK(rectangle.getPointCount() == 4);
+        CHECK(rectangle.getPoint(0) == sf::Vector2f(0, 0));
+        CHECK(rectangle.getPoint(1) == sf::Vector2f(9, 0));
+        CHECK(rectangle.getPoint(2) == sf::Vector2f(9, 8));
+        CHECK(rectangle.getPoint(3) == sf::Vector2f(0, 8));
+    }
+
+    SUBCASE("Set size")
+    {
+        sf::RectangleShape rectangle({7, 6});
+        rectangle.setSize({5, 4});
+        CHECK(rectangle.getSize() == sf::Vector2f(5, 4));
+    }
+}
