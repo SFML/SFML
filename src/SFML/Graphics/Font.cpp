0a251d15e3322b15c9e9cdfd5f10750d6641b5a7
kimci86 <kimci86@hotmail.fr>
2022-04-12 15:25:47 +0200

    Fix font pages not being created with the desired smoothness

--- src/SFML/Graphics/Font.cpp
+++ src/SFML/Graphics/Font.cpp
@@ -348,7 +348,7 @@ const Font::Info& Font::getInfo() const
 const Glyph& Font::getGlyph(Uint32 codePoint, unsigned int characterSize, bool bold, float outlineThickness) const
 {
     // Get the page corresponding to the character size
-    GlyphTable& glyphs = m_pages[characterSize].glyphs;
+    GlyphTable& glyphs = loadPage(characterSize).glyphs;
 
     // Build the key by combining the glyph index (based on code point), bold flag, and outline thickness
     Uint64 key = combine(outlineThickness, bold, FT_Get_Char_Index(static_cast<FT_Face>(m_face), codePoint));
@@ -476,7 +476,7 @@ float Font::getUnderlineThickness(unsigned int characterSize) const
 ////////////////////////////////////////////////////////////
 const Texture& Font::getTexture(unsigned int characterSize) const
 {
-    return m_pages[characterSize].texture;
+    return loadPage(characterSize).texture;
 }
 
 ////////////////////////////////////////////////////////////
@@ -568,6 +568,18 @@ void Font::cleanup()
 
 
 ////////////////////////////////////////////////////////////
+Font::Page& Font::loadPage(unsigned int characterSize) const
+{
+    // TODO: Remove this method and use try_emplace instead when updating to C++17
+    PageTable::iterator pageIterator = m_pages.find(characterSize);
+    if (pageIterator == m_pages.end())
+        pageIterator = m_pages.insert(std::make_pair(characterSize, Page(m_isSmooth))).first;
+
+    return pageIterator->second;
+}
+
+
+////////////////////////////////////////////////////////////
 Glyph Font::loadGlyph(Uint32 codePoint, unsigned int characterSize, bool bold, float outlineThickness) const
 {
     // The glyph to return
@@ -652,7 +664,7 @@ Glyph Font::loadGlyph(Uint32 codePoint, unsigned int characterSize, bool bold, f
         height += 2 * padding;
 
         // Get the glyphs page corresponding to the character size
-        Page& page = m_pages[characterSize];
+        Page& page = loadPage(characterSize);
 
         // Find a good position for the new glyph into the texture
         glyph.textureRect = findGlyphRect(page, width, height);
@@ -842,7 +854,7 @@ bool Font::setCurrentSize(unsigned int characterSize) const
 
 
 ////////////////////////////////////////////////////////////
-Font::Page::Page() :
+Font::Page::Page(bool smooth) :
 nextRow(3)
 {
     // Make sure that the texture is initialized by default
@@ -856,7 +868,7 @@ nextRow(3)
 
     // Create the texture
     texture.loadFromImage(image);
-    texture.setSmooth(true);
+    texture.setSmooth(smooth);
 }
 
 } // namespace sf
