badb3164136bba4bd75b05f5fcd4c6a0ddaef3fa
Bambo-Borris <canyoufeelit4@gmail.com>
2022-06-24 02:13:55 +0100

    Add tests for `sf::MemoryInputStream`

---
+++ test/System/MemoryInputStream.cpp
@@ -0,0 +1,33 @@
+#include <SFML/System/MemoryInputStream.hpp>
+#include <string_view>
+#include <ostream>
+
+#include <doctest.h>
+
+TEST_CASE("sf::MemoryInputStream class - [system]")
+{
+    SUBCASE("Empty stream")
+    {
+        sf::MemoryInputStream mis;
+
+        CHECK(mis.read(nullptr, 0) == -1);
+        CHECK(mis.seek(0) == -1);
+        CHECK(mis.tell() == -1);
+        CHECK(mis.getSize() == -1);
+    }
+
+    SUBCASE("Open memory stream")
+    {
+        using namespace std::literals::string_view_literals;
+        constexpr auto memoryContents = "hello world"sv;
+        sf::MemoryInputStream mis;
+        mis.open(memoryContents.data(), sizeof(char) * memoryContents.size());
+
+        char buffer[32];
+        CHECK(mis.read(buffer, 5) == 5);
+        CHECK(std::string_view(buffer, 5) == std::string_view(memoryContents.data(), 5));
+        CHECK(mis.seek(10) == 10);
+        CHECK(mis.tell() == 10);
+        CHECK(mis.getSize() == 11);
+    }
+}
