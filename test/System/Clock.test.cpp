053fdbdd6a12211beeabed4c5d9ff8293fd53b0f
MetGang <patrick_stritch@wp.pl>
2022-07-20 14:40:45 +0200

    Make `sf::Clock` pausable

--- test/System/Clock.test.cpp
+++ test/System/Clock.test.cpp
@@ -9,6 +9,8 @@
 
 TEST_CASE("[System] sf::Clock")
 {
+    using namespace std::chrono_literals;
+
     SECTION("Type traits")
     {
         STATIC_CHECK(std::is_copy_constructible_v<sf::Clock>);
@@ -17,21 +19,50 @@ TEST_CASE("[System] sf::Clock")
         STATIC_CHECK(std::is_nothrow_move_assignable_v<sf::Clock>);
     }
 
+    SECTION("Construction")
+    {
+        const sf::Clock clock;
+        CHECK(clock.isRunning());
+        CHECK(clock.getElapsedTime() >= sf::microseconds(0));
+    }
+
     SECTION("getElapsedTime()")
     {
         const sf::Clock clock;
         CHECK(clock.getElapsedTime() >= sf::microseconds(0));
         const auto elapsed = clock.getElapsedTime();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::this_thread::sleep_for(1ms);
         CHECK(clock.getElapsedTime() > elapsed);
+    }
+
+    SECTION("start/stop")
+    {
+        sf::Clock clock;
+        clock.stop();
+        CHECK(!clock.isRunning());
+        const auto elapsed = clock.getElapsedTime();
+        std::this_thread::sleep_for(1ms);
+        CHECK(elapsed == clock.getElapsedTime());
+
+        clock.start();
+        CHECK(clock.isRunning());
+        CHECK(clock.getElapsedTime() >= elapsed);
     }
 
     SECTION("restart()")
     {
         sf::Clock clock;
         CHECK(clock.restart() >= sf::microseconds(0));
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        CHECK(clock.isRunning());
+        std::this_thread::sleep_for(1ms);
         const auto elapsed = clock.restart();
         CHECK(clock.restart() < elapsed);
+    }
+
+    SECTION("reset()")
+    {
+        sf::Clock clock;
+        CHECK(clock.reset() >= sf::microseconds(0));
+        CHECK(!clock.isRunning());
     }
 }
