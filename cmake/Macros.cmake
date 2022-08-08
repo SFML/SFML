a098260d4f829f9f6188ea211b4489f79f95fdfb
Chris Thrasher <chrisjthrasher@gmail.com>
2022-08-07 20:00:12 -0600

    Remove unnecessary variable assignment
    This same code already exists in src/CMakeLists.txt

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -403,7 +403,6 @@ function(sfml_find_package)
         message(FATAL_ERROR "Unknown arguments when calling sfml_find_package: ${THIS_UNPARSED_ARGUMENTS}")
     endif()
 
-    set(CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake/Modules/")
     if (SFML_OS_IOS)
         find_host_package(${target} REQUIRED)
     else()
