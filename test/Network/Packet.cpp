28a4a8883281846f64079d007c904de4948df609
Chris Thrasher <chrisjthrasher@gmail.com>
2022-09-03 18:26:23 -0600

    Add more tests for `sf::Packet`

--- test/Network/Packet.cpp
+++ test/Network/Packet.cpp
@@ -2,20 +2,62 @@
 
 #include <doctest/doctest.h>
 
+#include <array>
 #include <limits>
 
-#define CHECK_PACKET_STREAM_OPERATORS(expected) \
-    do                                          \
-    {                                           \
-        sf::Packet packet;                      \
-        packet << expected;                     \
-        decltype(expected) received;            \
-        packet >> received;                     \
-        CHECK(expected == received);            \
+#define CHECK_PACKET_STREAM_OPERATORS(expected)              \
+    do                                                       \
+    {                                                        \
+        sf::Packet packet;                                   \
+        packet << expected;                                  \
+        CHECK(packet.getReadPosition() == 0);                \
+        CHECK(packet.getData() != nullptr);                  \
+        CHECK(packet.getDataSize() == sizeof(expected));     \
+        CHECK(!packet.endOfPacket());                        \
+        CHECK(static_cast<bool>(packet));                    \
+                                                             \
+        decltype(expected) received;                         \
+        packet >> received;                                  \
+        CHECK(packet.getReadPosition() == sizeof(expected)); \
+        CHECK(packet.getData() != nullptr);                  \
+        CHECK(packet.getDataSize() == sizeof(expected));     \
+        CHECK(packet.endOfPacket());                         \
+        CHECK(static_cast<bool>(packet));                    \
+        CHECK(expected == received);                         \
     } while (false)
 
 TEST_CASE("sf::Packet class - [network]")
 {
+    SUBCASE("Default constructor")
+    {
+        const sf::Packet packet;
+        CHECK(packet.getReadPosition() == 0);
+        CHECK(packet.getData() == nullptr);
+        CHECK(packet.getDataSize() == 0);
+        CHECK(packet.endOfPacket());
+        CHECK(static_cast<bool>(packet));
+    }
+
+    SUBCASE("Append and clear")
+    {
+        constexpr std::array data = {1, 2, 3, 4, 5, 6};
+
+        sf::Packet packet;
+        packet.append(data.data(), data.size());
+        CHECK(packet.getReadPosition() == 0);
+        CHECK(packet.getData() != nullptr);
+        CHECK(packet.getDataSize() == data.size());
+        CHECK(!packet.endOfPacket());
+        CHECK(static_cast<bool>(packet));
+
+        packet.clear();
+        CHECK(packet.getReadPosition() == 0);
+        CHECK(packet.getData() == nullptr);
+        CHECK(packet.getDataSize() == 0);
+        CHECK(packet.endOfPacket());
+        CHECK(static_cast<bool>(packet));
+    }
+
     SUBCASE("Stream operators")
     {
         SUBCASE("std::int8_t")
