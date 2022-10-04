754f0822b807a0f5f75b69bd7138a5171a124109
mamedov <vagif_m@bk.ru>
2022-10-04 23:48:50 +0300

    modifided

--- src/SFML/Audio/ALCheck.cpp
+++ src/SFML/Audio/ALCheck.cpp
@@ -6,6 +6,7 @@
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
 //
+
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it freely,
 // subject to the following restrictions:
@@ -38,6 +39,8 @@
 
 namespace
 {
+
+
 // A nested named namespace is used here to allow unity builds of SFML.
 namespace AlCheckImpl
 {
