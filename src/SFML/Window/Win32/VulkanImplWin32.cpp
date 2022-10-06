e948a5f41e112dc1d9daac9844682e89b0d0b093
Vittorio Romeo <vittorio.romeo@outlook.com>
2022-10-06 19:27:50 +0200

    Improve static initialization of vectors

--- src/SFML/Window/Win32/VulkanImplWin32.cpp
+++ src/SFML/Window/Win32/VulkanImplWin32.cpp
@@ -183,14 +183,7 @@ VulkanFunctionPointer VulkanImplWin32::getFunction(const char* name)
 ////////////////////////////////////////////////////////////
 const std::vector<const char*>& VulkanImplWin32::getGraphicsRequiredInstanceExtensions()
 {
-    static std::vector<const char*> extensions;
-
-    if (extensions.empty())
-    {
-        extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
-        extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
-    }
-
+    static const std::vector<const char*> extensions{VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_WIN32_SURFACE_EXTENSION_NAME};
     return extensions;
 }
 
