3e1031e3d4debc7e248a67dd054b3c3a6271b7a5
Mario Liebisch <mario.liebisch@gmail.com>
2022-10-25 10:18:35 +0200

    Pushed minimum Windows version to Windows 7

--- src/SFML/System/Win32/WindowsHeader.hpp
+++ src/SFML/System/Win32/WindowsHeader.hpp
@@ -34,15 +34,15 @@
 #endif
 
 #ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0501
+#define _WIN32_WINDOWS 0x0601
 #endif
 
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501
+#define _WIN32_WINNT 0x0601
 #endif
 
 #ifndef WINVER
-#define WINVER 0x0501
+#define WINVER 0x0601
 #endif
 
 #include <windows.h>
