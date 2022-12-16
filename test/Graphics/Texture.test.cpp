b128fcd3df3caacb7207f8e847e596ecfcd865a4
Chris Thrasher <chrisjthrasher@gmail.com>
2022-12-16 01:14:47 -0700

    Add tests for `sf::Texture`

--- test/Graphics/Texture.test.cpp
+++ test/Graphics/Texture.test.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics/Texture.hpp>
 
+#include <doctest/doctest.h>
+
+#include <GraphicsUtil.hpp>
 #include <type_traits>
 
 static_assert(std::is_copy_constructible_v<sf::Texture>);
@@ -8,3 +11,16 @@ static_assert(std::is_move_constructible_v<sf::Texture>);
 static_assert(!std::is_nothrow_move_constructible_v<sf::Texture>);
 static_assert(std::is_move_assignable_v<sf::Texture>);
 static_assert(!std::is_nothrow_move_assignable_v<sf::Texture>);
+
+TEST_CASE("[Graphics] sf::Texture")
+{
+    SUBCASE("Construction")
+    {
+        sf::Texture texture;
+        CHECK(texture.getSize() == sf::Vector2u());
+        CHECK(!texture.isSmooth());
+        CHECK(!texture.isSrgb());
+        CHECK(!texture.isRepeated());
+        CHECK(texture.getNativeHandle() == 0);
+    }
+}
