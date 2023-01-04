7929d60ab8465475668b8b43842605580a52fda8
HelloShiv <swagatswarooparida@gmail.com>
2023-01-04 22:53:06 +0530

    Removed unnecessary return EXIT_SUCCESS

--- examples/sound_capture/SoundCapture.cpp
+++ examples/sound_capture/SoundCapture.cpp
@@ -97,6 +97,4 @@ int main()
     // Wait until the user presses 'enter' key
     std::cout << "Press enter to exit..." << std::endl;
     std::cin.ignore(10000, '\n');
-
-    return EXIT_SUCCESS;
 }
