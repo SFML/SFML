72342f7e706974083bbd11fe4325f4d1f4007015
Chris Thrasher <chrisjthrasher@gmail.com>
2023-01-05 17:40:09 -0700

    Prevent using a temporary `sf::Texture` with `sf::Sprite`

--- test/Graphics/Sprite.test.cpp
+++ test/Graphics/Sprite.test.cpp
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 
+static_assert(!std::is_constructible_v<sf::Sprite, sf::Texture&&>);
 static_assert(std::is_copy_constructible_v<sf::Sprite>);
 static_assert(std::is_copy_assignable_v<sf::Sprite>);
 static_assert(std::is_nothrow_move_constructible_v<sf::Sprite>);
