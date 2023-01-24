8a307df494d1a8934384d8861cad3a1b9dbaaaba
Chris Thrasher <chrisjthrasher@gmail.com>
2023-01-23 23:15:58 -0700

    Update copyright year

--- src/SFML/Window/DRM/WindowImplDRM.cpp
+++ src/SFML/Window/DRM/WindowImplDRM.cpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2020 Andrew Mickelson
+// Copyright (C) 2023 Andrew Mickelson
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
