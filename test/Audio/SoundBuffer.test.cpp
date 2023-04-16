9722fb3724a6789c1b9258ade7eb89da59e27597
Chris Thrasher <chrisjthrasher@gmail.com>
2023-04-15 20:50:24 -0600

    Add tests for Audio module types

--- test/Audio/SoundBuffer.test.cpp
+++ test/Audio/SoundBuffer.test.cpp
@@ -1,10 +1,147 @@
 #include <SFML/Audio/SoundBuffer.hpp>
 
+// Other 1st party headers
+#include <SFML/System/FileInputStream.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <AudioUtil.hpp>
+#include <SystemUtil.hpp>
+#include <array>
 #include <type_traits>
 
-static_assert(std::is_copy_constructible_v<sf::SoundBuffer>);
-static_assert(std::is_copy_assignable_v<sf::SoundBuffer>);
-static_assert(std::is_move_constructible_v<sf::SoundBuffer>);
-static_assert(!std::is_nothrow_move_constructible_v<sf::SoundBuffer>);
-static_assert(std::is_move_assignable_v<sf::SoundBuffer>);
-static_assert(!std::is_nothrow_move_assignable_v<sf::SoundBuffer>);
+TEST_CASE("[Audio] sf::SoundBuffer", runAudioDeviceTests())
+{
+    SECTION("Type traits")
+    {
+        STATIC_CHECK(std::is_copy_constructible_v<sf::SoundBuffer>);
+        STATIC_CHECK(std::is_copy_assignable_v<sf::SoundBuffer>);
+        STATIC_CHECK(std::is_move_constructible_v<sf::SoundBuffer>);
+        STATIC_CHECK(!std::is_nothrow_move_constructible_v<sf::SoundBuffer>);
+        STATIC_CHECK(std::is_move_assignable_v<sf::SoundBuffer>);
+        STATIC_CHECK(!std::is_nothrow_move_assignable_v<sf::SoundBuffer>);
+    }
+
+    SECTION("Construction")
+    {
+        const sf::SoundBuffer soundBuffer;
+        CHECK(soundBuffer.getSamples() == nullptr);
+        CHECK(soundBuffer.getSampleCount() == 0);
+        CHECK(soundBuffer.getSampleRate() == 44100);
+        CHECK(soundBuffer.getChannelCount() == 1);
+        CHECK(soundBuffer.getDuration() == sf::Time::Zero);
+    }
+
+    SECTION("Copy semantics")
+    {
+        sf::SoundBuffer soundBuffer;
+        REQUIRE(soundBuffer.loadFromFile("Audio/ding.flac"));
+
+        SECTION("Construction")
+        {
+            const sf::SoundBuffer soundBufferCopy(soundBuffer); // NOLINT(performance-unnecessary-copy-initialization)
+            CHECK(soundBufferCopy.getSamples() != nullptr);
+            CHECK(soundBufferCopy.getSampleCount() == 87798);
+            CHECK(soundBufferCopy.getSampleRate() == 44100);
+            CHECK(soundBufferCopy.getChannelCount() == 1);
+            CHECK(soundBufferCopy.getDuration() == sf::microseconds(1990884));
+        }
+
+        SECTION("Assignment")
+        {
+            sf::SoundBuffer soundBufferCopy;
+            soundBufferCopy = soundBuffer;
+            CHECK(soundBufferCopy.getSamples() != nullptr);
+            CHECK(soundBufferCopy.getSampleCount() == 87798);
+            CHECK(soundBufferCopy.getSampleRate() == 44100);
+            CHECK(soundBufferCopy.getChannelCount() == 1);
+            CHECK(soundBufferCopy.getDuration() == sf::microseconds(1990884));
+        }
+    }
+
+    SECTION("loadFromFile()")
+    {
+        sf::SoundBuffer soundBuffer;
+
+        SECTION("Invalid filename")
+        {
+            CHECK(!soundBuffer.loadFromFile("does/not/exist.wav"));
+        }
+
+        SECTION("Valid file")
+        {
+            REQUIRE(soundBuffer.loadFromFile("Audio/ding.flac"));
+            CHECK(soundBuffer.getSamples() != nullptr);
+            CHECK(soundBuffer.getSampleCount() == 87798);
+            CHECK(soundBuffer.getSampleRate() == 44100);
+            CHECK(soundBuffer.getChannelCount() == 1);
+            CHECK(soundBuffer.getDuration() == sf::microseconds(1990884));
+        }
+    }
+
+    SECTION("loadFromMemory()")
+    {
+        sf::SoundBuffer soundBuffer;
+
+        SECTION("Invalid memory")
+        {
+            CHECK(!soundBuffer.loadFromMemory(nullptr, 0));
+            constexpr std::array<std::byte, 5> memory{};
+            CHECK(!soundBuffer.loadFromMemory(memory.data(), memory.size()));
+        }
+
+        SECTION("Valid memory")
+        {
+            const auto memory = loadIntoMemory("Audio/ding.flac");
+            REQUIRE(soundBuffer.loadFromMemory(memory.data(), memory.size()));
+            CHECK(soundBuffer.getSamples() != nullptr);
+            CHECK(soundBuffer.getSampleCount() == 87798);
+            CHECK(soundBuffer.getSampleRate() == 44100);
+            CHECK(soundBuffer.getChannelCount() == 1);
+            CHECK(soundBuffer.getDuration() == sf::microseconds(1990884));
+        }
+    }
+
+    SECTION("loadFromStream()")
+    {
+        sf::FileInputStream stream;
+        sf::SoundBuffer     soundBuffer;
+
+        SECTION("Invalid stream")
+        {
+            CHECK(!soundBuffer.loadFromStream(stream));
+        }
+
+        SECTION("Valid stream")
+        {
+            REQUIRE(stream.open("Audio/ding.flac"));
+            REQUIRE(soundBuffer.loadFromStream(stream));
+            CHECK(soundBuffer.getSamples() != nullptr);
+            CHECK(soundBuffer.getSampleCount() == 87798);
+            CHECK(soundBuffer.getSampleRate() == 44100);
+            CHECK(soundBuffer.getChannelCount() == 1);
+            CHECK(soundBuffer.getDuration() == sf::microseconds(1990884));
+        }
+    }
+
+    SECTION("saveToFile()")
+    {
+        const auto filename = std::filesystem::temp_directory_path() / "ding.flac";
+
+        {
+            sf::SoundBuffer soundBuffer;
+            REQUIRE(soundBuffer.loadFromFile("Audio/ding.flac"));
+            REQUIRE(soundBuffer.saveToFile(filename));
+        }
+
+        sf::SoundBuffer soundBuffer;
+        REQUIRE(soundBuffer.loadFromFile(filename));
+        CHECK(soundBuffer.getSamples() != nullptr);
+        CHECK(soundBuffer.getSampleCount() == 87798);
+        CHECK(soundBuffer.getSampleRate() == 44100);
+        CHECK(soundBuffer.getChannelCount() == 1);
+        CHECK(soundBuffer.getDuration() == sf::microseconds(1990884));
+
+        CHECK(std::filesystem::remove(filename));
+    }
+}
--- test/Audio/SoundSource.test.cpp
+++ test/Audio/SoundSource.test.cpp
@@ -1,10 +1,134 @@
 #include <SFML/Audio/SoundSource.hpp>
 
+#include <catch2/catch_test_macros.hpp>
+
+#include <AudioUtil.hpp>
+#include <SystemUtil.hpp>
 #include <type_traits>
 
-static_assert(!std::is_constructible_v<sf::SoundSource>);
-static_assert(!std::is_copy_constructible_v<sf::SoundSource>);
-static_assert(std::is_copy_assignable_v<sf::SoundSource>);
-static_assert(!std::is_move_constructible_v<sf::SoundSource>);
-static_assert(std::is_move_assignable_v<sf::SoundSource>);
-static_assert(!std::is_nothrow_move_assignable_v<sf::SoundSource>);
+namespace
+{
+class SoundSource : public sf::SoundSource
+{
+    void play() override
+    {
+    }
+
+    void pause() override
+    {
+    }
+
+    void stop() override
+    {
+    }
+
+    void* getSound() const override
+    {
+        return {};
+    }
+
+public:
+    Status getStatus() const override
+    {
+        return {};
+    }
+};
+} // namespace
+
+TEST_CASE("[Audio] sf::SoundSource", runAudioDeviceTests())
+{
+    SECTION("Type traits")
+    {
+        STATIC_CHECK(!std::is_constructible_v<sf::SoundSource>);
+        STATIC_CHECK(!std::is_copy_constructible_v<sf::SoundSource>);
+        STATIC_CHECK(std::is_copy_assignable_v<sf::SoundSource>);
+        STATIC_CHECK(!std::is_move_constructible_v<sf::SoundSource>);
+        STATIC_CHECK(std::is_move_assignable_v<sf::SoundSource>);
+        STATIC_CHECK(!std::is_nothrow_move_assignable_v<sf::SoundSource>);
+        STATIC_CHECK(std::has_virtual_destructor_v<sf::SoundSource>);
+    }
+
+    SECTION("Construction")
+    {
+        const SoundSource soundSource;
+        CHECK(soundSource.getPitch() == 0);
+        CHECK(soundSource.getVolume() == 0);
+        CHECK(soundSource.getPosition() == sf::Vector3f());
+        CHECK(!soundSource.isRelativeToListener());
+        CHECK(soundSource.getMinDistance() == 0);
+        CHECK(soundSource.getAttenuation() == 0);
+        CHECK(soundSource.getStatus() == sf::SoundSource::Status::Stopped);
+    }
+
+    SECTION("Copy semantics")
+    {
+        const SoundSource soundSource;
+
+        SECTION("Construction")
+        {
+            const SoundSource soundSourceCopy(soundSource); // NOLINT(performance-unnecessary-copy-initialization)
+            CHECK(soundSourceCopy.getPitch() == 0);
+            CHECK(soundSourceCopy.getVolume() == 0);
+            CHECK(soundSourceCopy.getPosition() == sf::Vector3f());
+            CHECK(!soundSourceCopy.isRelativeToListener());
+            CHECK(soundSourceCopy.getMinDistance() == 0);
+            CHECK(soundSourceCopy.getAttenuation() == 0);
+            CHECK(soundSourceCopy.getStatus() == sf::SoundSource::Status::Stopped);
+        }
+
+        SECTION("Assignment")
+        {
+            SoundSource soundSourceCopy;
+            soundSourceCopy = soundSource;
+            CHECK(soundSourceCopy.getPitch() == 0);
+            CHECK(soundSourceCopy.getVolume() == 0);
+            CHECK(soundSourceCopy.getPosition() == sf::Vector3f());
+            CHECK(!soundSourceCopy.isRelativeToListener());
+            CHECK(soundSourceCopy.getMinDistance() == 0);
+            CHECK(soundSourceCopy.getAttenuation() == 0);
+            CHECK(soundSourceCopy.getStatus() == sf::SoundSource::Status::Stopped);
+        }
+    }
+
+    SECTION("Set/get pitch")
+    {
+        SoundSource soundSource;
+        soundSource.setPitch(42);
+        CHECK(soundSource.getPitch() == 0);
+    }
+
+    SECTION("Set/get volume")
+    {
+        SoundSource soundSource;
+        soundSource.setVolume(0.5f);
+        CHECK(soundSource.getVolume() == 0);
+    }
+
+    SECTION("Set/get position")
+    {
+        SoundSource soundSource;
+        soundSource.setPosition({1, 2, 3});
+        CHECK(soundSource.getPosition() == sf::Vector3f());
+    }
+
+    SECTION("Set/get relative to listener")
+    {
+        SoundSource soundSource;
+        soundSource.setRelativeToListener(true);
+        CHECK(!soundSource.isRelativeToListener());
+    }
+
+    SECTION("Set/get min distance")
+    {
+        SoundSource soundSource;
+        soundSource.setMinDistance(12.34f);
+        CHECK(soundSource.getMinDistance() == 0);
+    }
+
+    SECTION("Set/get attenuation")
+    {
+        SoundSource soundSource;
+        soundSource.setAttenuation(10);
+        CHECK(soundSource.getAttenuation() == 0);
+    }
+}
