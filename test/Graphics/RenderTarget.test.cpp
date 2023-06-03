3ee2a1e58a7c17e8326e51d733c3ca51aafe557e
Seppe <seppe.degryse@telenet.be>
2023-06-04 00:02:11 +0200

    Implemented move semantics for `sf::RenderTarget`

--- test/Graphics/RenderTarget.test.cpp
+++ test/Graphics/RenderTarget.test.cpp
@@ -2,8 +2,30 @@
 
 #include <type_traits>
 
-static_assert(!std::is_constructible_v<sf::RenderTarget>);
-static_assert(!std::is_copy_constructible_v<sf::RenderTarget>);
-static_assert(!std::is_copy_assignable_v<sf::RenderTarget>);
-static_assert(!std::is_nothrow_move_constructible_v<sf::RenderTarget>);
-static_assert(!std::is_nothrow_move_assignable_v<sf::RenderTarget>);
+TEST_CASE("[Graphics] sf::RenderTarget", runDisplayTests())
+{
+    SECTION("Type traits")
+    {
+        STATIC_CHECK(!std::is_constructible_v<sf::RenderTarget>);
+        STATIC_CHECK(!std::is_copy_constructible_v<sf::RenderTarget>);
+        STATIC_CHECK(!std::is_copy_assignable_v<sf::RenderTarget>);
+        STATIC_CHECK(std::is_nothrow_move_constructible_v<sf::RenderTarget>);
+        STATIC_CHECK(std::is_nothrow_move_assignable_v<sf::RenderTarget>);
+    }
+
+    SECTION("Move semantics")
+    {
+        SUBCASE("Move constructor")
+        {
+            sf::RenderTarget        moveRenderTarget;
+            const sf::RenderTarget  renderTarget(std::move(moveRenderTarget));
+        }
+
+        SUBCASE("Move assignment")
+        {
+            sf::RenderTarget        moveRenderTarget;
+            const sf::RenderTarget  renderTarget;
+            moveRenderTarget = std::move(renderTarget);
+        }
+    }
+}
