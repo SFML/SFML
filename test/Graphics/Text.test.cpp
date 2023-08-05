371af2b50fd8272c6539e495ccbd907750388722
kimci86 <kimci86@hotmail.fr>
2023-08-05 20:22:06 +0200

    Update Text constructor static assert to use correct parameters

--- test/Graphics/Text.test.cpp
+++ test/Graphics/Text.test.cpp
@@ -2,7 +2,7 @@
 
 #include <type_traits>
 
-static_assert(!std::is_constructible_v<sf::Text, sf::String, sf::Font&&, unsigned int>);
+static_assert(!std::is_constructible_v<sf::Text, sf::Font&&, sf::String, unsigned int>);
 static_assert(std::is_copy_constructible_v<sf::Text>);
 static_assert(std::is_copy_assignable_v<sf::Text>);
 static_assert(std::is_nothrow_move_constructible_v<sf::Text>);
