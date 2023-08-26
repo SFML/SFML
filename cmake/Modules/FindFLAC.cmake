2c7ed96654c67ce15c96d81d253991a74afdc879
Chris Thrasher <chrisjthrasher@gmail.com>
2023-08-26 13:30:05 -0600

    Add `FLAC::FLAC` target

--- cmake/Modules/FindFLAC.cmake
+++ cmake/Modules/FindFLAC.cmake
@@ -16,3 +16,11 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FLAC DEFAULT_MSG FLAC_LIBRARY FLAC_INCLUDE_DIR)
 
 mark_as_advanced(FLAC_INCLUDE_DIR FLAC_LIBRARY)
+
+add_library(FLAC::FLAC IMPORTED UNKNOWN)
+set_target_properties(FLAC::FLAC PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${FLAC_INCLUDE_DIR})
+if(FLAC_LIBRARY MATCHES "/([^/]+)\\.framework$")
+    set_target_properties(FLAC::FLAC PROPERTIES IMPORTED_LOCATION ${FLAC_LIBRARY}/${CMAKE_MATCH_1})
+else()
+    set_target_properties(FLAC::FLAC PROPERTIES IMPORTED_LOCATION ${FLAC_LIBRARY})
+endif()
