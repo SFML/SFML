f39204f2da3d090de661f039b9ee7b71a24236d7
Chris Thrasher <chrisjthrasher@gmail.com>
2023-08-26 23:59:04 -0600

    Add tests for `sf::Http`
    This class hides a lot of its logic in the private section of the
    classes and uses friends to access them so there is little accessible
    in the public interface for testing.

--- test/Network/Http.test.cpp
+++ test/Network/Http.test.cpp
@@ -1,8 +1,48 @@
 #include <SFML/Network/Http.hpp>
 
+#include <catch2/catch_test_macros.hpp>
+
 #include <type_traits>
 
-static_assert(!std::is_copy_constructible_v<sf::Http>);
-static_assert(!std::is_copy_assignable_v<sf::Http>);
-static_assert(!std::is_nothrow_move_constructible_v<sf::Http>);
-static_assert(!std::is_nothrow_move_assignable_v<sf::Http>);
+TEST_CASE("[Network] sf::Http")
+{
+    SECTION("Type traits")
+    {
+        STATIC_CHECK(!std::is_copy_constructible_v<sf::Http>);
+        STATIC_CHECK(!std::is_copy_assignable_v<sf::Http>);
+        STATIC_CHECK(!std::is_nothrow_move_constructible_v<sf::Http>);
+        STATIC_CHECK(!std::is_nothrow_move_assignable_v<sf::Http>);
+    }
+
+    SECTION("Request")
+    {
+        SECTION("Type traits")
+        {
+            STATIC_CHECK(std::is_copy_constructible_v<sf::Http::Request>);
+            STATIC_CHECK(std::is_copy_assignable_v<sf::Http::Request>);
+            STATIC_CHECK(std::is_move_constructible_v<sf::Http::Request>);
+            STATIC_CHECK(std::is_nothrow_move_assignable_v<sf::Http::Request>);
+        }
+    }
+
+    SECTION("Response")
+    {
+        SECTION("Type traits")
+        {
+            STATIC_CHECK(std::is_copy_constructible_v<sf::Http::Response>);
+            STATIC_CHECK(std::is_copy_assignable_v<sf::Http::Response>);
+            STATIC_CHECK(std::is_move_constructible_v<sf::Http::Response>);
+            STATIC_CHECK(std::is_nothrow_move_assignable_v<sf::Http::Response>);
+        }
+
+        SECTION("Construction")
+        {
+            const sf::Http::Response response;
+            CHECK(response.getField("").empty());
+            CHECK(response.getStatus() == sf::Http::Response::Status::ConnectionFailed);
+            CHECK(response.getMajorHttpVersion() == 0);
+            CHECK(response.getMinorHttpVersion() == 0);
+            CHECK(response.getBody().empty());
+        }
+    }
+}
