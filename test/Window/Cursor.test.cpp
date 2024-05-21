51e620bffdf9058bb6a8a40e18ab54891ea7f969
Chris Thrasher <chrisjthrasher@gmail.com>
2024-05-20 19:04:40 -0600

    Remove default empty state of `sf::Cursor`

--- test/Window/Cursor.test.cpp
+++ test/Window/Cursor.test.cpp
@@ -10,60 +10,38 @@ TEST_CASE("[Window] sf::Cursor", runDisplayTests())
 {
     SECTION("Type traits")
     {
+        STATIC_CHECK(!std::is_default_constructible_v<sf::Cursor>);
         STATIC_CHECK(!std::is_copy_constructible_v<sf::Cursor>);
         STATIC_CHECK(!std::is_copy_assignable_v<sf::Cursor>);
         STATIC_CHECK(std::is_nothrow_move_constructible_v<sf::Cursor>);
         STATIC_CHECK(std::is_nothrow_move_assignable_v<sf::Cursor>);
     }
 
-    SECTION("Construction")
-    {
-        const sf::Cursor cursor;
-    }
-
-    SECTION("Move semantics")
-    {
-        SECTION("Construction")
-        {
-            sf::Cursor       movedCursor;
-            const sf::Cursor cursor(std::move(movedCursor));
-        }
-
-        SECTION("Assignment")
-        {
-            sf::Cursor movedCursor;
-            sf::Cursor cursor;
-            cursor = std::move(movedCursor);
-        }
-    }
-
     SECTION("loadFromPixels()")
     {
-        sf::Cursor                  cursor;
-        std::array<std::uint8_t, 4> pixels{};
+        static constexpr std::array<std::uint8_t, 4> pixels{};
 
-        CHECK(!cursor.loadFromPixels(nullptr, {}, {}));
-        CHECK(!cursor.loadFromPixels(pixels.data(), {0, 1}, {}));
-        CHECK(!cursor.loadFromPixels(pixels.data(), {1, 0}, {}));
-        CHECK(cursor.loadFromPixels(pixels.data(), {1, 1}, {}));
+        CHECK(!sf::Cursor::loadFromPixels(nullptr, {}, {}));
+        CHECK(!sf::Cursor::loadFromPixels(pixels.data(), {0, 1}, {}));
+        CHECK(!sf::Cursor::loadFromPixels(pixels.data(), {1, 0}, {}));
+        CHECK(sf::Cursor::loadFromPixels(pixels.data(), {1, 1}, {}));
     }
 
     SECTION("loadFromSystem()")
     {
-        sf::Cursor cursor;
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::Hand));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeHorizontal));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeVertical));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeLeft));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeRight));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeTop));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeBottom));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeTopLeft));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeTopRight));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeBottomLeft));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::SizeBottomRight));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::Cross));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::Help));
-        CHECK(cursor.loadFromSystem(sf::Cursor::Type::NotAllowed));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::Hand));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeHorizontal));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeVertical));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeLeft));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeRight));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeTop));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeBottom));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeTopLeft));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeTopRight));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeBottomLeft));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::SizeBottomRight));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::Cross));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::Help));
+        CHECK(sf::Cursor::loadFromSystem(sf::Cursor::Type::NotAllowed));
     }
 }
