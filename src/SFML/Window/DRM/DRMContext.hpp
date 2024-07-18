74816d3149536b968cbf3dbb26250474bc4fd4cf
Andrew Mickelson <andrew.mickelson@gmail.com>
2024-07-17 20:15:09 -0700

    [drm] Fix DRM mode setting to use SFML window dimensions
        - actually use the dimensions of the SFML window as the DRM mode
          dimensions when setting a DRM mode.
        - If SFML window dimensions don't match a valid DRM mode, show
          the window contents on screen in the current mode instead of
          simply failing to show anything.
        - With these change the SFML examples are now working for me

--- src/SFML/Window/DRM/DRMContext.hpp
+++ src/SFML/Window/DRM/DRMContext.hpp
@@ -143,11 +143,10 @@ public:
     ///
     /// \param width   Back buffer width, in pixels
     /// \param height  Back buffer height, in pixels
-    /// \param bpp     Pixel depth, in bits per pixel
     /// \param scanout True to present the surface to the screen
     ///
     ////////////////////////////////////////////////////////////
-    void createSurface(unsigned int width, unsigned int height, unsigned int bpp, bool scanout);
+    void createSurface(unsigned int width, unsigned int height, bool scanout);
 
     ////////////////////////////////////////////////////////////
     /// \brief Destroy the EGL surface
@@ -210,8 +209,6 @@ private:
     gbm_bo* m_currentBO;
     gbm_bo* m_nextBO;
     gbm_surface* m_gbmSurface;
-    unsigned int m_width;
-    unsigned int m_height;
     bool m_shown;
     bool m_scanOut;
 };
