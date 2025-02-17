8bed3bca8747d43f2edaaac714c5e341f10b04ad
JonnyPtn <jonathan.r.paton@googlemail.com>
2025-02-17 19:56:05 +0000

    Set required properties for ios test apps

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -47,6 +47,8 @@ function(sfml_set_common_ios_properties target)
             MACOSX_BUNDLE ON # Bare executables are not usable on iOS, only bundle applications
             MACOSX_BUNDLE_GUI_IDENTIFIER "org.sfml-dev.${target}" # If missing, trying to launch an example in simulator will make Xcode < 9.3 crash
             MACOSX_BUNDLE_BUNDLE_NAME "${target}"
+            MACOSX_BUNDLE_BUNDLE_VERSION "${PROJECT_VERSION}"
+            MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION_MAJOR}"
             MACOSX_BUNDLE_LONG_VERSION_STRING "${PROJECT_VERSION}"
         )
     endif()
@@ -406,6 +408,11 @@ function(sfml_add_test target SOURCES DEPENDS)
         if(SFML_OS_ANDROID)
             set_target_properties(${target} PROPERTIES CROSSCOMPILING_EMULATOR "${PROJECT_BINARY_DIR}/run-in-adb-shell.sh")
         endif()
+    endif()
+
+    # Required to actually run the tests
+    if(SFML_OS_IOS)
+        sfml_set_common_ios_properties(${target})
     endif()
 
     # Add the test
