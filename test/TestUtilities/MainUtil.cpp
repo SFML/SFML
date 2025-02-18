630fc42eb84ee087931355a9d08e5d4362c971ec
JonnyPtn <jonathan.r.paton@googlemail.com>
2025-02-18 21:59:19 +0000

    To run unit tests on iOS we must use SFML's main module and must include the assets in the app bundle

---
+++ test/TestUtilities/MainUtil.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_session.hpp>
+
+#include <SFML/Main.hpp>
+
+#include <array>
+
+////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    return Catch::Session().run(argc, argv);
+}
