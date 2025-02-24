eb2a1a92ace737656ffad35db5873621321c8d7e
Mark Jansen <mark.jansen@reactos.org>
2025-02-24 19:41:06 +0100

    Add the clang-tidy folder as hint for finding run-clang-tidy

--- cmake/Tidy.cmake
+++ cmake/Tidy.cmake
@@ -22,7 +22,8 @@ endif()
 # Find Python and run-clang-tidy script
 find_package(Python 3 REQUIRED)
 
-find_program(RUN_CLANG_TIDY run-clang-tidy)
+get_filename_component(CLANG_TIDY_DIR ${CLANG_TIDY_EXECUTABLE} DIRECTORY)
+find_program(RUN_CLANG_TIDY run-clang-tidy HINTS ${CLANG_TIDY_DIR})
 if(NOT RUN_CLANG_TIDY)
     message(FATAL_ERROR "Failed to find run-clang-tidy script")
 endif()
