095979c30c6dbd83e089f2b3d246ce99bf4109f8
JonnyPtn <jonathan.r.paton@googlemail.com>
2025-05-01 17:25:51 +0100

    Some further fixes from latest master

--- test/main.cpp
+++ test/main.cpp
@@ -4,7 +4,7 @@
 #include <array>
 
 ////////////////////////////////////////////////////////////
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
     return Catch::Session().run(argc, argv);
 }
