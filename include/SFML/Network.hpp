a1da40b170bf40b59cdfa7780646db62970da1e8
binary1248 <binary1248@hotmail.com>
2025-08-21 03:07:54 +0200

    Added support for SFTP as a replacement for FTP, since FTP does not support secure connections.

--- include/SFML/Network.hpp
+++ include/SFML/Network.hpp
@@ -32,6 +32,7 @@
 #include <SFML/Network/Http.hpp>
 #include <SFML/Network/IpAddress.hpp>
 #include <SFML/Network/Packet.hpp>
+#include <SFML/Network/Sftp.hpp>
 #include <SFML/Network/Socket.hpp>
 #include <SFML/Network/SocketHandle.hpp>
 #include <SFML/Network/SocketSelector.hpp>
