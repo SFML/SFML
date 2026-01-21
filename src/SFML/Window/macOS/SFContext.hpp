1433fd41e1522c8ae8a0025c41e2baf4902d02c3
Jonny Paton <jonathan.r.paton@googlemail.com>
2026-01-21 11:45:04 +0000

    Remove redundant documentation comments in private implementation files

--- src/SFML/Window/macOS/SFContext.hpp
+++ src/SFML/Window/macOS/SFContext.hpp
@@ -56,97 +56,35 @@ using NSWindowRef        = void*;
 namespace sf::priv
 {
 ////////////////////////////////////////////////////////////
-/// \brief macOS (Cocoa) implementation of OpenGL contexts
-///
-////////////////////////////////////////////////////////////
 class SFContext : public GlContext
 {
 public:
     ////////////////////////////////////////////////////////////
-    /// \brief Create a new context, not associated to a window
-    ///
-    /// \param shared Context to share the new one with (can be a null pointer)
-    ///
-    ////////////////////////////////////////////////////////////
     SFContext(SFContext* shared);
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Create a new context attached to a window
-    ///
-    /// \param shared       Context to share the new one with
-    /// \param settings     Creation parameters
-    /// \param owner        Pointer to the owner window
-    /// \param bitsPerPixel Pixel depth, in bits per pixel
-    ///
     ////////////////////////////////////////////////////////////
     SFContext(SFContext* shared, const ContextSettings& settings, const WindowImpl& owner, unsigned int bitsPerPixel);
 
     ////////////////////////////////////////////////////////////
-    /// \brief Create a new context that embeds its own rendering target
-    ///
-    /// \param shared   Context to share the new one with
-    /// \param settings Creation parameters
-    /// \param size     Back buffer width and height, in pixels
-    ///
-    ////////////////////////////////////////////////////////////
     SFContext(SFContext* shared, const ContextSettings& settings, Vector2u size);
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Destructor
-    ///
     ////////////////////////////////////////////////////////////
     ~SFContext() override;
 
     ////////////////////////////////////////////////////////////
-    /// \brief Get the address of an OpenGL function
-    ///
-    /// \param name Name of the function to get the address of
-    ///
-    /// \return Address of the OpenGL function, `nullptr` on failure
-    ///
-    ////////////////////////////////////////////////////////////
     static GlFunctionPointer getFunction(const char* name);
 
     ////////////////////////////////////////////////////////////
-    /// \brief Display what has been rendered to the context so far
-    ///
-    ////////////////////////////////////////////////////////////
     void display() override;
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Enable or disable vertical synchronization
-    ///
-    /// Activating vertical synchronization will limit the number
-    /// of frames displayed to the refresh rate of the monitor.
-    /// This can avoid some visual artifacts, and limit the framerate
-    /// to a good value (but not constant across different computers).
-    ///
-    /// \param enabled `true` to enable v-sync, `false` to deactivate
-    ///
     ////////////////////////////////////////////////////////////
     void setVerticalSyncEnabled(bool enabled) override;
 
 protected:
     ////////////////////////////////////////////////////////////
-    /// \brief Activate the context as the current target
-    ///        for rendering
-    ///
-    /// \param current Whether to make the context current or no longer current
-    ///
-    /// \return `true` on success, `false` if any error happened
-    ///
-    ////////////////////////////////////////////////////////////
     bool makeCurrent(bool current) override;
 
 private:
-    ////////////////////////////////////////////////////////////
-    /// \brief Create the context
-    /// \note Must only be called from Ctor.
-    ///
-    /// \param shared       Context to share the new one with (can be a null pointer)
-    /// \param bitsPerPixel bpp
-    /// \param settings     Creation parameters
-    ///
     ////////////////////////////////////////////////////////////
     void createContext(SFContext* shared, unsigned int bitsPerPixel, const ContextSettings& settings);
 
