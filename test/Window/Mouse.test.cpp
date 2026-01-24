73996231381f4b8ac08b28c7e59cbbce9963d65f
JonnyPtn <jonathan.r.paton@googlemail.com>
2026-01-24 21:55:59 +0000

    Use CGWarpMouseCursorPosition to set mouse position on mac and add test for it

--- test/Window/Mouse.test.cpp
+++ test/Window/Mouse.test.cpp
@@ -14,4 +14,11 @@ TEST_CASE("[Window] sf::Mouse", runDisplayTests())
         CHECK(!sf::Mouse::isButtonPressed(sf::Mouse::Button::Extra1));
         CHECK(!sf::Mouse::isButtonPressed(sf::Mouse::Button::Extra2));
     }
+
+    SECTION("setPosition() and getPosition()")
+    {
+        sf::Vector2i pos{12, 42};
+        sf::Mouse::setPosition(pos);
+        CHECK(sf::Mouse::getPosition() == pos);
+    }
 }
